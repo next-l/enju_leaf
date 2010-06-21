c36b204fc4da14dc5df30c07e93884313181f7b5
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-06-21 14:31:55 +0900

    default is PostgreSQL

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,3 +1,10 @@
 module ResourcesHelper
   include ManifestationsHelper
+
+  def resource_title(resource, action)
+    string = 'Enju Light - '
+    unless action == 'index'
+      string << resource.original_title
+    end
+  end
 end
--- app/models/import_request.rb
+++ app/models/import_request.rb
@@ -1,10 +1,25 @@
 class ImportRequest < ActiveRecord::Base
+  include AASM
   belongs_to :user
   belongs_to :manifestation, :class_name => 'Resource'
   validates_presence_of :isbn
   validate :check_isbn
   validate :check_imported, :on => :create
   enju_ndl
+
+  aasm_column :state
+  aasm_state :pending
+  aasm_state :failed
+  aasm_state :completed
+  aasm_initial_state :pending
+
+  aasm_event :aasm_fail do
+    transitions :from => :pending, :to => :failed
+  end
+
+  aasm_event :aasm_complete do
+    transitions :from => :pending, :to => :completed
+  end
 
   def check_isbn
     if isbn.present?
@@ -32,9 +47,14 @@ class ImportRequest < ActiveRecord::Base
   end
 
   def import!
-    manifestation = self.class.import_isbn!(isbn)
-    self.manifestation = manifestation
-    save!
+    unless manifestation
+      manifestation = self.class.import_isbn!(isbn)
+      self.manifestation = manifestation
+      aasm_complete!
+      manifestation.index!
+    else
+      aasm_fail!
+    end
   end
 
 end
