6ba4f738ce7cd3211370b8a88d8bfbd9e02fa73c
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-07-13 21:36:12 +0900

    updated template files

--- app/models/resource_sweeper.rb
+++ app/models/resource_sweeper.rb
@@ -96,8 +96,9 @@ class ResourceSweeper < ActionController::Caching::Sweeper
       expire_editable_fragment(record.item, 'holding')
     when record.is_a?(Language)
       Language.all.each do |language|
+        expire_fragment(:page => 'header', :locale => language.iso_639_1)
+        expire_fragment(:page => 'select_locale', :locale => language.iso_639_1)
         expire_fragment(:controller => 'page', :locale => language.iso_639_1)
-        expire_fragment(:controller => 'page', :locale => language.iso_639_1, :page => 'select_locale')
       end
     when record.is_a?(SeriesStatement)
       record.manifestations.each do |manifestation|
