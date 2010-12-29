a44529ebbe069fd665e7e5c342e2dc6ef49a2a2e
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-12-29 22:38:26 +0900

    fixed related_manifestation id

--- vendor/plugins/enju_amazon/lib/enju_amazon.rb
+++ vendor/plugins/enju_amazon/lib/enju_amazon.rb
@@ -69,24 +69,6 @@ module EnjuAmazon
       nil
     end
 
-    def amazon_customer_reviews
-      if self.amazon
-        doc = Nokogiri::XML(self.amazon)
-        reviews = []
-        doc.xpath('//xmlns:Review').each do |item|
-          r = {
-            :date => item.at('Date').inner_text,
-            :summary => item.at('Summary').inner_text,
-            :content => item.at('Content').inner_text
-          }
-          reviews << r
-        end
-        reviews
-      else
-        []
-      end
-    end
-
     def asin
       if self.isbn.length == 10
         self.isbn
