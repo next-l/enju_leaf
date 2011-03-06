24d117cc3448b662846dffcf2f23d23d3d7c0632
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-03-06 17:29:32 +0900

    added dalli option for Passenger

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EnjuLeaf::Application.initialize!
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # Only works with DalliStore
+    Rails.cache.reset if forked
+  end
+end
