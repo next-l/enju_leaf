fbfaafe96c12f1a52b8a15e7f85c8bcaf87fff2a
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-04-26 03:11:25 +0900

    updated menu

--- app/models/series_statement.rb
+++ app/models/series_statement.rb
@@ -2,7 +2,6 @@ class SeriesStatement < ActiveRecord::Base
   has_many :manifestations
   validates_presence_of :original_title
   acts_as_list
-
   searchable do
     text :title do
       original_title
@@ -11,8 +10,9 @@ class SeriesStatement < ActiveRecord::Base
     integer :manifestation_ids, :multiple => true
   end
 
+  normalize_attributes :original_title, :issn
+
   def last_issue
     manifestations.first(:conditions => 'date_of_publication IS NOT NULL', :order => 'date_of_publication DESC') || manifestations.first
   end
-
 end
