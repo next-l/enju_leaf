fdbd0f27cbd8747597dfcb946c591dba827e912d
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-05-22 04:42:53 +0900

    added Gemfile.lock

--- app/controllers/message_templates_controller.rb
+++ app/controllers/message_templates_controller.rb
@@ -1,7 +1,6 @@
 class MessageTemplatesController < InheritedResources::Base
   respond_to :html, :xml
   before_filter :check_client_ip_address
-  before_filter :prepare_options
   load_and_authorize_resource
 
   def update
