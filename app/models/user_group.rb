37a21b95f117f5e8f5628d5cbeb019806095c2cf
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-06-30 09:41:57 +0900

    added validations

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -9,7 +9,11 @@ class UserGroup < ActiveRecord::Base
   has_many :checkout_types, :through => :user_group_has_checkout_types, :order => :position
   has_many :lending_policies
 
-  validates_numericality_of :valid_period_for_new_user, :greater_than_or_equal_to => 0
+  validates_numericality_of :valid_period_for_new_user,
+    :number_of_day_to_notify_due_date,
+    :number_of_day_to_notify_overdue,
+    :number_of_time_to_notify_overdue,
+    :greater_than_or_equal_to => 0
 
   def self.per_page
     10
