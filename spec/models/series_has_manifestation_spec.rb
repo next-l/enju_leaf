0e3794be0cc4e7681dc2729dcd075dee4f019c7a
Kosuke Tanabe <kosuke@okayama.email.ne.jp>
2011-09-12 07:36:01 +0900

    updated spec files

--- spec/models/series_has_manifestation_spec.rb
+++ spec/models/series_has_manifestation_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SeriesHasManifestation do
   it "should reindex" do
-    series_has_manifestation = Factory.create(:series_has_manifestation)
+    series_has_manifestation = FactoryGirl.create(:series_has_manifestation)
     series_has_manifestation.reindex.should be_true
   end
 end
--- spec/models/series_statement_merge_list_spec.rb
+++ spec/models/series_statement_merge_list_spec.rb
@@ -4,7 +4,7 @@ describe SeriesStatementMergeList do
   fixtures :series_statements
 
   it "should merge series_statment" do
-    series_statement_merge_list = Factory.create(:series_statement_merge_list)
+    series_statement_merge_list = FactoryGirl.create(:series_statement_merge_list)
     series_statement_merge_list.merge_series_statements(SeriesStatement.all).should be_true
   end
 end
