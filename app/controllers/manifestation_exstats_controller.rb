d3637c046b517a3a13ad36fbdd35e3d5d8b1c289
Masayoshi Hagio <hagio@opentech.co.jp>
2011-10-20 16:28:05 +0900

    ベストリーダ・リクエスト対応

--- app/controllers/manifestation_exstats_controller.rb
+++ app/controllers/manifestation_exstats_controller.rb
@@ -10,7 +10,19 @@ class ManifestationExstatsController < ApplicationController
   # bestreader checkout
   def bestreader
     @title = t('page.best_reader')
-    @manifestations = Manifestation.find(:all, :limit => 10)
+    @start_d = Time.zone.now - 30.days
+    @end_d = Time.zone.now
+    @limit = 10
+
+    @manifestations_tmp = Manifestation.find(:all, :include => {:items => :checkouts}, :limit => @limit, :conditions => ['checkouts.created_at >= ? AND checkouts.created_at < ?', @start_d, @end_d])
+
+    #@manifestations_tmp.each_with_index do |m, i|
+	#logger.info "#{i} #{m.original_title}, #{m.items[0].checkouts.size}"
+    #end
+
+    @manifestations = @manifestations_tmp.sort {|a, b|
+	b.items[0].checkouts.size <=> a.items[0].checkouts.size
+    }
 
     respond_to do |format|
       format.html # index.html.erb
@@ -21,7 +33,19 @@ class ManifestationExstatsController < ApplicationController
   # bestrequest reserve
   def bestrequest
     @title = t('page.best_request')
-    @manifestations = Manifestation.find(:all, :limit => 10)
+    @start_d = Time.zone.now - 30.days
+    @end_d = Time.zone.now
+    @limit = 10
+
+    @manifestations_tmp = Manifestation.find(:all, :include => :reserves, :limit => @limit, :conditions => ['reserves.created_at >= ? AND reserves.created_at < ?', @start_d, @end_d])
+
+    @manifestations = @manifestations_tmp.sort {|a, b|
+	b.reserves.size <=> a.reserves.size
+    }
+
+    #@manifestations.each_with_index do |m, i|
+	#logger.info "#{i} #{m.original_title}, #{m.reserves.size}"
+    #end
 
     respond_to do |format|
       format.html # index.html.erb
