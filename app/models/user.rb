94570fbee5f0e918ef3252ad5dfa3dca5ba93232
reiko <reiko@enjudev1.(none)>
2011-10-25 18:26:34 +0900

    modified edit_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ActiveRecord::Base
 
 #  validates_presence_of :email, :email_confirmation, :on => :create #, :if => proc{|user| !user.operator.try(:has_role?, 'Librarian')}
   validates_associated :patron, :user_group, :library
-  validates_presence_of :user_group, :library, :locale, :full_name #, :user_number
+  validates_presence_of :user_group, :library, :locale #, :user_number
   validates :user_number, :uniqueness => true, :format => {:with => /\A[0-9A-Za-z_]+\Z/}, :allow_blank => true
   validates_confirmation_of :email #, :on => :create, :if => proc{|user| !user.operator.try(:has_role?, 'Librarian')}
   before_validation :set_role_and_patron, :on => :create
