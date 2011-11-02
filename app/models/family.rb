9999be45528164f34eeee1a0518724788bfcdc9c
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2011-11-02 19:48:24 +0900

    refs 2204 入力チェック

--- app/models/family.rb
+++ app/models/family.rb
@@ -3,6 +3,15 @@ class Family < ActiveRecord::Base
   has_many :family_users
 
   def add_user(user_ids)
+    # TODO :need refactoring
+    if user_ids.nil? || user_ids.empty? 
+      logger.debug "family users no record"
+      #errors.add(:manifestation_id, I18n.t('family.no_select_users'))
+      errors.add(:base, I18n.t('family.no_select_users'))
+      raise 
+    end
+
+    #
     user_ids.each do |user_id|
       self.users << User.find(user_id) rescue nil
     end
