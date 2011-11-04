cd955b12640a323a0ac3264fd4824efa033aeb2c
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2011-11-04 21:20:19 +0900

    refs 2219 bugfix

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,6 +2,12 @@ class Term < ActiveRecord::Base
   validates_presence_of :display_name, :start_at, :end_at
   before_save :set_end_date
   belongs_to :budget
+
+  def validate
+    unless self.start_at < self.end_at
+      errors.add(:base, I18n.t('activerecord.attributes.term.end_at_invalid'))
+    end
+  end
  
   def set_end_date
     self.end_at = self.end_at.end_of_day
