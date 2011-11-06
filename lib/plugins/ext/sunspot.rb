8862a0db652a318a2371a7cf7e45bb34f1b640d8
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-11-06 15:58:05 +0900

    sunspot patch to add encoding

--- lib/plugins/ext/sunspot.rb
+++ lib/plugins/ext/sunspot.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'sunspot', 'dismax'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'sunspot', 'start_record'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'sunspot', 'abstract_search'))
--- lib/plugins/ext/sunspot/dismax.rb
+++ lib/plugins/ext/sunspot/dismax.rb
@@ -1,29 +1,22 @@
 module Sunspot
   module Query
     class Dismax
-      attr_writer :minimum_match, :phrase_slop, :query_phrase_slop, :tie
-
-      def initialize(keywords)
-        @keywords = keywords
-        @fulltext_fields = {}
-        @boost_queries = []
-        @highlights = []
-      end
-
-      #
-      # The query as Solr parameters
-      #
       def to_params
         params = { :q => @keywords }
         params[:fl] = '* score'
         params[:qf] = @fulltext_fields.values.map { |field| field.to_boosted_field }.join(' ')
-        #params[:defType] = 'dismax'
+        params[:defType] = 'lucene'
         if @phrase_fields
           params[:pf] = @phrase_fields.map { |field| field.to_boosted_field }.join(' ')
         end
         unless @boost_queries.empty?
           params[:bq] = @boost_queries.map do |boost_query|
             boost_query.to_boolean_phrase
+          end
+        end
+        unless @boost_functions.empty?
+          params[:bf] = @boost_functions.map do |boost_function|
+            boost_function.to_s
           end
         end
         if @minimum_match
@@ -42,46 +35,6 @@ module Sunspot
           Sunspot::Util.deep_merge!(params, highlight.to_params)
         end
         params
-      end
-
-      #
-      # Assign a new boost query and return it.
-      #
-      def create_boost_query(factor)
-        @boost_queries << boost_query = BoostQuery.new(factor)
-        boost_query
-      end
-
-      #
-      # Add a fulltext field to be searched, with optional boost
-      #
-      def add_fulltext_field(field, boost = nil)
-        @fulltext_fields[field.indexed_name] = TextFieldBoost.new(field, boost)
-      end
-
-      #
-      # Add a phrase field for extra boost
-      #
-      def add_phrase_field(field, boost = nil)
-        @phrase_fields ||= []
-        @phrase_fields << TextFieldBoost.new(field, boost)
-      end
-
-      #
-      # Set highlighting options for the query. If fields is empty, the
-      # Highlighting object won't pass field names at all, which means
-      # the dismax's :qf parameter will be used by Solr.
-      #
-      def add_highlight(fields=[], options={})
-        @highlights << Highlighting.new(fields, options)
-      end
-
-      #
-      # Determine if a given field is being searched. Used by DSL to avoid
-      # overwriting boost parameters when injecting defaults.
-      #
-      def has_fulltext_field?(field)
-        @fulltext_fields.has_key?(field.indexed_name)
       end
     end
   end
