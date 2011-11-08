98bb6788441540d4cb33f7b88870bb5a364d047b
emiko <emiko@opentech.co.jp>
2011-11-08 12:15:08 +0900

    update budget

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,7 +2,7 @@ class Budget < ActiveRecord::Base
   validates_presence_of :library_id, :term_id
   has_one :library, :dependent => :destroy
   has_one :term
-  validates_numericality_of :amount
+  validates_numericality_of :amount, :allow_blank => true
 
   def library
     Library.find(self.library_id) 
