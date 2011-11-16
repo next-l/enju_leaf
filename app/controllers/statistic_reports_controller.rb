d74a3a6af84e2605f5d79ddd25a6da6296b6145c
emiko <emiko@opentech.co.jp>
2011-11-16 19:07:02 +0900

    refs 2312 update to create monthly statistic

--- app/controllers/statistic_reports_controller.rb
+++ app/controllers/statistic_reports_controller.rb
@@ -2,6 +2,7 @@ class StatisticReportsController < ApplicationController
   require 'thinreports'
 
   def index
+    @year = Time.zone.now.years_ago(1).strftime("%Y")
   end
 
   def monthly_data
@@ -9,6 +10,12 @@ class StatisticReportsController < ApplicationController
 
   def get_monthly_report
     term = params[:term]
+    unless term =~ /^\d{4}$/
+      flash[:message] = t('statistic_report.invalid_year')
+      @year = term
+      render :index
+      return false
+    end
     libraries = Library.all
     begin 
       report = ThinReports::Report.new :layout => "#{Rails.root.to_s}/app/views/statistic_reports/monthly_report"
@@ -46,7 +53,214 @@ class StatisticReportsController < ApplicationController
         end
       end
 
-      send_data report.generate, :filename => "#{term}_#{configatron.statistic_report.monthly}", :type => 'application/pdf'
+      # checkout users all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.checkout_users'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1220, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1220, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # checkout users each library
+      libraries.each do |library|
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1220, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1220, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+      end
+
+      # daily average of checkout users all library
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.average_checkout_users'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1223, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1223, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # daily average of checkout users each library
+      libraries.each do |library|
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1223, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1223, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+      end
+
+      # all users all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.users'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        row.item(:option).value(t('statistic_report.all_users'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1120, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1120, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # unlocked users all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:option).value(t('statistic_report.unlocked_users'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1121, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1121, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # locked users all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:option).value(t('statistic_report.locked_users'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1122, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1122, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+
+      # users each library
+      libraries.each do |library|
+        # all users
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          row.item(:option).value(t('statistic_report.all_users'))
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1120, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1120, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+        # unlocked users
+        report.page.list(:list).add_row do |row|
+          row.item(:option).value(t('statistic_report.unlocked_users'))
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1121, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1121, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+        # locked users
+        report.page.list(:list).add_row do |row|
+          row.item(:option).value(t('statistic_report.locked_users'))
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1122, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1122, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+      end
+
+      # reserves all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.reserves'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1330, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1330, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # reserves each library
+      libraries.each do |library|
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1330, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1330, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+      end
+
+      # questions all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.questions'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        12.times do |t|
+          if t < 4 # for Japanese fiscal year
+            value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1430, :library_id => 0).first.value rescue 0
+          else
+            value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1430, :library_id => 0).first.value rescue 0
+          end
+          row.item("value#{t+1}").value(value)
+          row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+        end  
+      end
+      # questions each library
+      libraries.each do |library|
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          12.times do |t|
+            if t < 4 # for Japanese fiscal year
+              value = Statistic.where(:yyyymm => "#{term.to_i + 1}#{"%02d" % (t + 1)}", :data_type => 1430, :library_id => library.id).first.value rescue 0 
+            else
+              value = Statistic.where(:yyyymm => "#{term}#{"%02d" % (t + 1)}", :data_type => 1430, :library_id => library.id).first.value rescue 0 
+            end
+            row.item("value#{t+1}").value(value)
+            row.item("valueall").value(value) if t == 2 # March(end of fiscal year)
+          end  
+        end
+      end
+
+
+      send_data report.generate, :filename => "#{term}_#{configatron.statistic_report.monthly}", :type => 'application/pdf', :disposition => 'attachment'
       return true
     rescue Exception => e
       logger.error "failed #{e}"
