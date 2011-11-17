2cfcb05c7669ef1078c4e1acce2d89969bf4a012
emiko <emiko@opentech.co.jp>
2011-11-17 16:42:14 +0900

    refs 2312 update to create statistic for Sun-Sat

--- app/controllers/statistic_reports_controller.rb
+++ app/controllers/statistic_reports_controller.rb
@@ -1,11 +1,17 @@
 class StatisticReportsController < ApplicationController
   require 'thinreports'
+  before_filter :check_role
 
   def index
     @year = Time.zone.now.years_ago(1).strftime("%Y")
+    @month = Time.zone.now.months_ago(1).strftime("%Y%m")
   end
 
-  def monthly_data
+  # check role
+  def check_role
+    unless current_user.try(:has_role?, 'Librarian')
+      access_denied; return
+    end
   end
 
   def get_monthly_report
@@ -13,6 +19,7 @@ class StatisticReportsController < ApplicationController
     unless term =~ /^\d{4}$/
       flash[:message] = t('statistic_report.invalid_year')
       @year = term
+      @month = Time.zone.now.months_ago(1).strftime("%Y%m")
       render :index
       return false
     end
@@ -266,5 +273,143 @@ class StatisticReportsController < ApplicationController
       logger.error "failed #{e}"
       return false
     end	
+  end
+
+  def get_day_report
+    start_at = params[:start_at]
+    end_at = params[:end_at]
+    end_at = start_at if end_at.empty?
+    unless (start_at =~ /^\d{6}$/ && end_at =~ /^\d{6}$/) && start_at.to_i <= end_at.to_i 
+      flash[:message] = t('statistic_report.invalid_month')
+      @year = Time.zone.now.months_ago(1).strftime("%Y")
+      @month = Time.zone.now.months_ago(1).strftime("%Y%m")
+      @start_at = start_at
+      @end_at = end_at
+      render :index
+      return false
+    end
+    libraries = Library.all
+    logger.error "creating statistic: #{start_at} - #{end_at}"
+
+    begin
+      report = ThinReports::Report.new :layout => "#{Rails.root.to_s}/app/views/statistic_reports/day_report"
+      report.start_new_page
+       
+      report.page.item(:year).value(start_at[0,4])
+      report.page.item(:year_start_at).value(start_at[0,4])
+      report.page.item(:month_start_at).value(start_at[4,6])
+      report.page.item(:year_end_at).value(end_at[0,4])
+      report.page.item(:month_end_at).value(end_at[4,6])
+
+
+      # checkout users all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.checkout_users'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        sum = 0
+        7.times do |t|
+          value = 0
+          datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2220 AND library_id = ? AND day = ?", 0, t])
+          datas.each do |data|
+            value = value + data.value
+          end
+          sum = sum + value
+          row.item("value#{t}").value(value)
+        end
+        row.item("valueall").value(sum)  
+      end
+      # checkout users each libraries
+      libraries.each do |library|
+        sum = 0
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          7.times do |t|
+            value = 0
+            datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2220 AND library_id = ? AND day = ?", library.id, t])
+            datas.each do |data|
+              value = value + data.value
+            end
+            sum = sum + value
+            row.item("value#{t}").value(value)
+          end
+          row.item("valueall").value(sum)
+        end
+      end
+
+      # reserves all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.reserves'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        sum = 0
+        7.times do |t|
+          value = 0
+          datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2330 AND library_id = ? AND day = ?", 0, t])
+          datas.each do |data|
+            value = value + data.value
+          end
+          sum = sum + value
+          row.item("value#{t}").value(value)
+        end
+        row.item("valueall").value(sum)  
+      end
+      # reserves each libraries
+      libraries.each do |library|
+        sum = 0
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          7.times do |t|
+            value = 0
+            datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2330 AND library_id = ? AND day = ?", library.id, t])
+            datas.each do |data|
+              value = value + data.value
+            end
+            sum = sum + value
+            row.item("value#{t}").value(value)
+          end
+          row.item("valueall").value(sum)
+        end
+      end
+
+      # questions all libraries
+      report.page.list(:list).add_row do |row|
+        row.item(:type).value(t('statistic_report.questions'))
+        row.item(:library).value(t('statistic_report.all_library'))
+        sum = 0
+        7.times do |t|
+          value = 0
+          datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2430 AND library_id = ? AND day = ?", 0, t])
+          datas.each do |data|
+            value = value + data.value
+          end
+          sum = sum + value
+          row.item("value#{t}").value(value)
+        end
+        row.item("valueall").value(sum)  
+      end
+      # reserves each libraries
+      libraries.each do |library|
+        sum = 0
+        report.page.list(:list).add_row do |row|
+          row.item(:library).value(library.display_name)
+          7.times do |t|
+            value = 0
+            datas = Statistic.where(["yyyymm >= #{start_at} AND yyyymm <= #{end_at} AND data_type = 2430 AND library_id = ? AND day = ?", library.id, t])
+            datas.each do |data|
+              value = value + data.value
+            end
+            sum = sum + value
+            row.item("value#{t}").value(value)
+          end
+          row.item("valueall").value(sum)
+        end
+      end
+
+
+      send_data report.generate, :filename => "#{start_at}_#{end_at}_#{configatron.statistic_report.day}", :type => 'application/pdf', :disposition => 'attachment'
+      return true
+    rescue Exception => e
+      logger.error "failed #{e}"
+      return false
+    end
   end
 end
