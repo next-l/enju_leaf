041fac0b79ef65f8bcffd2a67d163441640cd021
reiko <reiko@enjudev1.(none)>
2011-11-18 16:23:48 +0900

    refs 2026 modified unablelist

--- app/controllers/unablelist_controller.rb
+++ app/controllers/unablelist_controller.rb
@@ -1,26 +1,22 @@
 class UnablelistController < ApplicationController
+  def initialize
+    @select_librarlies = Library.find(:all).collect{|i| [ i.display_name, i.id ] }
+    @selected_library = nil
+    super
+  end
+
   def index
     return access_denied unless (user_signed_in? and current_user.has_role?('Librarian'))
 
-    sort = 'user_number asc'
-    case params[:sort_by]
-    when 'library'
-      sort = 'library_id asc, user_number asc'
-    end
-    @users = User.where(:unable => true).order(sort)
+    @selected_library = params[:library][:id] unless params[:library].blank?
+    @users = unablelist_users(@selected_library, params[:sort_by])
   end
 
   def output
     return access_denied unless (user_signed_in? and current_user.has_role?('Librarian'))
 
-    sort = 'user_number asc'
-    sort_by = t('activerecord.attributes.user.user_number')
-    if params[:sort_by] == 'library'
-      sort = 'library_id asc, user_number asc'
-      sort_by = t('activerecord.attributes.user.library')
-    end 
-    users = User.where(:unable => true).order(sort)
-    
+    users = unablelist_users(params[:library], params[:sort_by])
+
     require 'thinreports'
     report = ThinReports::Report.new :layout => File.join(Rails.root, 'report', 'unablelist.tlf')
 
@@ -34,13 +30,15 @@ class UnablelistController < ApplicationController
     end
 
     report.start_new_page do |page|
+      sort_by = t('activerecord.attributes.user.user_number')
+      sort_by = t('activerecord.attributes.user.library') if params[:sort_by] == 'library'
       page.item(:date).value(Time.now)
       page.item(:sort).value(t('activerecord.attributes.unablelist.output_format')+":"+sort_by)
 
       before_library = nil
       users.each do |user|
         page.list(:list).add_row do |row|
-          if params[:sort_by] == 'library' and before_library == user.library
+          if (params[:sort_by] == 'library' || !params[:library].blank? ) and before_library == user.library
            row.item(:library_line).hide
            row.item(:library).hide
           end
@@ -58,5 +56,18 @@ class UnablelistController < ApplicationController
     end
 
     send_data report.generate, :filename => configatron.unablelist_print.filename, :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  private
+  def unablelist_users(selected_library, sort_by)
+    sort = 'user_number asc'
+    if selected_library.nil? || selected_library.empty?
+      sort = 'library_id asc, user_number asc' if sort_by ==  'library'
+      @users = User.where(:unable => true).order(sort)
+    else
+      @users = User.where(:unable => true, :library_id => selected_library).order(sort)
+      params[:sort_by] = 'user_number'
+    end
+    return @users
   end
 end
