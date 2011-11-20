fe43429bdd2653cb6c3fec3c98a58400fde3c599
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-11-21 02:53:34 +0900

    fixed prefix search in OpenURL

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -505,60 +505,60 @@ class ManifestationsController < ApplicationController
     end
 
     if options[:mode] == 'recent'
-      query = "#{query} created_at_d: [NOW-1MONTH TO NOW]"
+      query = "#{query} created_at_d:[NOW-1MONTH TO NOW]"
     end
 
     #unless options[:carrier_type].blank?
-    #  query = "#{query} carrier_type_s: #{options[:carrier_type]}"
+    #  query = "#{query} carrier_type_s:#{options[:carrier_type]}"
     #end
 
     #unless options[:library].blank?
     #  library_list = options[:library].split.uniq.join(' and ')
-    #  query = "#{query} library_sm: #{library_list}"
+    #  query = "#{query} library_sm:#{library_list}"
     #end
 
     #unless options[:language].blank?
-    #  query = "#{query} language_sm: #{options[:language]}"
+    #  query = "#{query} language_sm:#{options[:language]}"
     #end
 
     #unless options[:subject].blank?
-    #  query = "#{query} subject_sm: #{options[:subject]}"
+    #  query = "#{query} subject_sm:#{options[:subject]}"
     #end
 
     unless options[:tag].blank?
-      query = "#{query} tag_sm: #{options[:tag]}"
+      query = "#{query} tag_sm:#{options[:tag]}"
     end
 
     unless options[:creator].blank?
-      query = "#{query} creator_text: #{options[:creator]}"
+      query = "#{query} creator_text:#{options[:creator]}"
     end
 
     unless options[:contributor].blank?
-      query = "#{query} contributor_text: #{options[:contributor]}"
+      query = "#{query} contributor_text:#{options[:contributor]}"
     end
 
     unless options[:isbn].blank?
-      query = "#{query} isbn_sm: #{options[:isbn]}"
+      query = "#{query} isbn_sm:#{options[:isbn]}"
     end
 
     unless options[:issn].blank?
-      query = "#{query} issn_sm: #{options[:issn]}"
+      query = "#{query} issn_s:#{options[:issn]}"
     end
 
     unless options[:lccn].blank?
-      query = "#{query} lccn_s: #{options[:lccn]}"
+      query = "#{query} lccn_s:#{options[:lccn]}"
     end
 
     unless options[:nbn].blank?
-      query = "#{query} nbn_s: #{options[:nbn]}"
+      query = "#{query} nbn_s:#{options[:nbn]}"
     end
 
     unless options[:publisher].blank?
-      query = "#{query} publisher_text: #{options[:publisher]}"
+      query = "#{query} publisher_text:#{options[:publisher]}"
     end
 
     unless options[:item_identifier].blank?
-      query = "#{query} item_identifier_sm: #{options[:item_identifier]}"
+      query = "#{query} item_identifier_sm:#{options[:item_identifier]}"
     end
 
     unless options[:number_of_pages_at_least].blank? and options[:number_of_pages_at_most].blank?
@@ -568,7 +568,7 @@ class ManifestationsController < ApplicationController
       number_of_pages[:at_least] = "*" if number_of_pages[:at_least] == 0
       number_of_pages[:at_most] = "*" if number_of_pages[:at_most] == 0
 
-      query = "#{query} number_of_pages_i: [#{number_of_pages[:at_least]} TO #{number_of_pages[:at_most]}]"
+      query = "#{query} number_of_pages_i:[#{number_of_pages[:at_least]} TO #{number_of_pages[:at_most]}]"
     end
 
     query = set_pub_date(query, options)
@@ -698,7 +698,7 @@ class ManifestationsController < ApplicationController
           pub_date[:to] = Time.zone.parse(Time.mktime(options[:pub_date_to]).to_s).end_of_year.utc.iso8601
         end
       end
-      query = "#{query} date_of_publication_d: [#{pub_date[:from]} TO #{pub_date[:to]}]"
+      query = "#{query} date_of_publication_d:[#{pub_date[:from]} TO #{pub_date[:to]}]"
     end
     query
   end
@@ -726,7 +726,7 @@ class ManifestationsController < ApplicationController
           acquisition_date[:to] = Time.zone.parse(Time.mktime(options[:acquired_to]).to_s).end_of_year.utc.iso8601
         end
       end
-      query = "#{query} acquired_at_d: [#{acquisition_date[:from]} TO #{acquisition_date[:to]}]"
+      query = "#{query} acquired_at_d:[#{acquisition_date[:from]} TO #{acquisition_date[:to]}]"
     end
     query
   end
@@ -739,7 +739,6 @@ class ManifestationsController < ApplicationController
       else
         @manifestation.original_title = @series_statement.original_title
         @manifestation.title_transcription = @series_statement.title_transcription
-        @manifestation.issn = @series_statement.issn
       end
     elsif @original_manifestation
       @manifestation.original_title = @original_manifestation.original_title
