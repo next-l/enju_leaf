fd8c53f263da3c7d722acac0e6b7f935c7afdc4f
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2011-11-22 11:03:51 +0900

    refs 2036,2037,2038 個々の資料についての表示用設定を追加

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -63,7 +63,7 @@ configatron.statistic_report.open = 9
 configatron.statistic_report.hours = 12 
 
 # 資料の詳細画面に過去の貸出回数を表示
-configatron.manifestation.display_checkout_counter = true
-configatron.manifestation.display_reserve_counter = true
-configatron.manifestation.display_last_checkout_date = true
+configatron.manifestation.display_checkouts_count = true
+configatron.manifestation.display_reserves_count = true
+configatron.manifestation.display_last_checkout_datetime = true
 
