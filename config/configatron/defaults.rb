7a22d4e2317c54e5a0775d3eccb559ef8ce93c4f
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2011-11-26 13:29:04 +0900

    refs 2326 サウンドファイル取得用ライブラリを追加

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -67,3 +67,11 @@ configatron.manifestation.display_checkouts_count = true
 configatron.manifestation.display_reserves_count = true
 configatron.manifestation.display_last_checkout_datetime = true
 
+# 音
+configatron.sounds.basedir = "sounds/"
+#configatron.sounds.errors.a = "chimeA08.ogg"
+#configatron.sounds.errors.b = "chimes01.ogg"
+#configatron.sounds.errors.c = "churchA08.ogg"
+#configatron.sounds.errors.d = "missA11.ogg"
+configatron.sounds.errors.checkin.not_checkin = "chimeA08.ogg"
+
