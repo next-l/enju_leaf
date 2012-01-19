ea1788d436ccc7e7589d2a21a52231bda6da82d4
reiko <reiko@enjudev1.(none)>
2012-01-19 14:34:37 +0900

    refs2082 modified tsv

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -56,8 +56,10 @@ configatron.manifestation_print.filename = "manifestation.pdf"
 # 貸出票の出力
 configatron.checkouts_print.filename = "checkouts.pdf"
 configatron.checkouts_print.message = "本は大事に扱いましょう。期限はきちんと守りましょう。インターネットで延長ができます。"
-# 貸出リストの出力
-configatron.checkoutlist_print.filename = "checkoutlist.pdf"
+# 貸出リスト(pdf)の出力
+configatron.checkoutlist_print_pdf.filename = "checkoutlist.pdf"
+# 貸出リスト(csv)の出力
+configatron.checkoutlist_print_csv.filename = "checkoutlist.csv"
 # 貸出リストに利用者の年齢を表示するかどうか
 configatron.checkout_print.old = true
 
