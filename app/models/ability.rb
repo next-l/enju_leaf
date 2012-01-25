bf2f08fdd56fdddc027813d3c830e32d4f56f53c
emiko <emiko@opentech.co.jp>
2012-01-25 17:20:48 +0900

    refs 2593 update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,7 +16,7 @@ class Ability
       can [:edit, :update, :destroy], EventCategory do |event_category|
         !['unknown', 'closed'].include?(event_category.name)
       end
-      can [:read, :create, :update, :export_loan_lists, :get_loan_lists, :pickup, :pickup_item, :accept, :accept_item, :download_file], InterLibraryLoan
+      can [:read, :create, :export_loan_lists, :get_loan_lists, :pickup, :pickup_item, :accept, :accept_item, :download_file], InterLibraryLoan
       can [:update, :destroy], InterLibraryLoan do |inter_library_loan|
         inter_library_loan.state == "pending" || inter_library_loan.state == "requested"
       end
@@ -164,8 +164,8 @@ class Ability
       can [:edit, :update, :destroy], EventCategory do |event_category|
         !['unknown', 'closed'].include?(event_category.name)
       end
-      can [:read, :create, :update, :export_loan_lists, :get_loan_lists, :pickup, :pickup_item, :accept, :accept_item, :download_file], InterLibraryLoan
-      can [:update, :destroy], InterLibraryLoan do |inter_library_loan|
+      can [:read, :create, :export_loan_lists, :get_loan_lists, :pickup, :pickup_item, :accept, :accept_item, :download_file], InterLibraryLoan
+      can [:update, :update, :destroy], InterLibraryLoan do |inter_library_loan|
         inter_library_loan.state == "pending" || inter_library_loan.state == "requested"
       end
       can [:read, :create, :update], Item
