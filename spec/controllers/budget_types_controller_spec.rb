e272839389937aa857ba370a67542dfec7268906
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-02-03 04:18:45 +0900

    fixed budget_type test

--- spec/controllers/budget_types_controller_spec.rb
+++ spec/controllers/budget_types_controller_spec.rb
@@ -19,6 +19,7 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe BudgetTypesController do
+  fixtures :library_groups
   login_admin
 
   # This should return the minimal set of attributes required to create a valid
