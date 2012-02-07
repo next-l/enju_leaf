caf2dc12be6c060cce9f7be43b0a95f90976a1d4
emiko <emiko@opentech.co.jp>
2012-02-07 18:17:54 +0900

    refs 1983 update to calculate expense with date

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -43,8 +43,8 @@ class ExpensesController < ApplicationController
     budgets = params[:budget]
     libraries = params[:library]
     bookstores = params[:bookstore]
-    term_from = Date.parse(params[:term_from]) rescue nil
-    term_to = Date.parse(params[:term_to]) rescue nil
+    term_from = parse_term(params[:term_from])
+    term_to = parse_term(params[:term_to])
     if term_to.nil? && term_from
       expenses = Expense.joins(:item).where(["expenses.budget_id IN (?) AND items.shelf_id IN (?) AND items.bookstore_id IN (?) AND expenses.created_at >= ?", budgets, Shelf.where(["library_id IN (?)", libraries]).map(&:id), bookstores, term_from])
     elsif term_from.nil? && term_to
@@ -71,10 +71,13 @@ class ExpensesController < ApplicationController
     budgets = params[:budgets]
     libraries = params[:libraries]
     bookstores = params[:bookstores]
-    term_from = Date.parse(params[:term_from]) rescue nil
-    term_to = Date.parse(params[:term_to]) rescue nil
+    term_from = parse_term(params[:term_from])
+    term_to = parse_term(params[:term_to])
     error = false
     list_size = 0
+
+    logger.error "term from: #{term_from}"
+    logger.error "term to: #{term_to}"
 
     # check checkbox
     error = true if libraries.blank? || budgets.blank? || bookstores.blank?
@@ -102,5 +105,23 @@ class ExpensesController < ApplicationController
     page = 1 if page == 0 and !error
 
     render :json => {:success => 1, :list_size => list_size, :page => page}
+  end
+
+private
+  def parse_term(term)
+    return nil if term.blank?
+    date = nil
+    begin
+      if term =~ /^\d{8}/
+        date = Time.zone.parse("#{term}")
+      elsif term =~ /^\d{6}/
+        date = Time.zone.parse("#{term}01")  
+      elsif term =~ /^\d{4}/
+        date = Time.zone.parse("#{term}0101")
+      end
+    rescue 
+      date = nil
+    end
+    return date
   end
 end
