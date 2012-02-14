666c2900266db2276216e81f85cb6b923b9d9bad
reiko <reiko@enjudev1.(none)>
2012-02-14 21:05:27 +0900

    refs2772 modified output_tsv

--- app/controllers/resource_import_results_controller.rb
+++ app/controllers/resource_import_results_controller.rb
@@ -1,5 +1,5 @@
 class ResourceImportResultsController < InheritedResources::Base
-  respond_to :html, :xml#, :csv
+  respond_to :html, :xml, :csv
   before_filter :access_denied, :except => [:index, :show]
   before_filter :check_client_ip_address
   load_and_authorize_resource
@@ -13,10 +13,11 @@ class ResourceImportResultsController < InheritedResources::Base
     else
       @results_num = @resource_import_results.length
     end
-    if params[:output_tsv]
-      data = ResourceImportResult.get_resource_import_results_tsv(@resource_import_results)
-      send_data data, :filename => configatron.resource_import_results_print_tsv.filename
-      return
+
+    if params[:format] == 'tsv'
+      respond_to do |format|
+        format.tsv { send_data ResourceImportResult.get_resource_import_results_tsv(@resource_import_results), :filename => configatron.resource_import_results_print_tsv.filename }
+      end
     else
       @resource_import_results = @resource_import_results.page(params[:page])
     end
