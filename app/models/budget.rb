4e2fb9063abd32aa032ac4c93d45e2d9b5ae2ae0
emiko <emiko@opentech.co.jp>
2012-02-15 16:42:48 +0900

    refs 2830

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,7 +2,7 @@ class Budget < ActiveRecord::Base
   validates_presence_of :library_id, :term_id
   has_one :library
   belongs_to :term
-  has_one :expense
+  has_many :expenses
   belongs_to :budget_type
   validates_numericality_of :amount, :allow_blank => true
 
