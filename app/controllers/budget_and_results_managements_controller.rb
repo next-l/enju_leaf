200cd2772671e371f6a16a86029b26f0d0b0520c
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-03-04 15:22:57 +0900

    refs 3064 予実対比にサマリー表、月別の詳細、TSV出力に対応

--- app/controllers/budget_and_results_managements_controller.rb
+++ app/controllers/budget_and_results_managements_controller.rb
@@ -1,13 +1,36 @@
 class BudgetAndResultsManagementsController < ApplicationController
+  def show
+    term_yyyymm01 = nil
+    unless params[:id]
+      access_denied; return
+    end
+    begin 
+      term_yyyymm01 = Date.strptime(params[:id], "%Y%m")
+    rescue ArgumentError 
+      access_denied; return
+    end
+
+    term_start_yyyymm = term_yyyymm01.strftime("%Y%m")
+    term_end_yyyymm = term_yyyymm01.end_of_month.strftime("%Y%m")
+
+    @expenses = Expense.where("acquired_at_ym BETWEEN ? AND ?", term_start_yyyymm, term_end_yyyymm).order(:acquired_at_ym)
+
+  end
+
   def index
     @term = Term.current_term
     @terms = Term.find(:all, :order => "start_at")
     @results = []
+    @budget_sum = nil
+    @expense_sum = nil
     
     if params[:id]
       term_id = params[:id]
       logger.info "term id=#{term_id}}"
 
+      @budget_sum = 0
+      @expense_sum = 0
+ 
       term = Term.find(term_id)
       @term = term
       term_start_yyyymm = term.start_at.strftime("%Y%m")
@@ -23,6 +46,7 @@ class BudgetAndResultsManagementsController < ApplicationController
         r[:created_at] = b.created_at.strftime("%Y%m%d")
         r[:type] = 1
         @results << r
+        @budget_sum = @budget_sum +  b.amount
       end
       @expenses.each do |k, v|
         r = {}
@@ -30,10 +54,13 @@ class BudgetAndResultsManagementsController < ApplicationController
         r[:expense] = v
         r[:created_at] = "#{k}99"
         r[:type] = 2
+        r[:ym] = k.to_s.slice(0, 6)
         @results << r
+        @expense_sum = @expense_sum + v
       end
 
-      #pp @results
+      @digestibility_sum = (@budget_sum == 0)?(0):((Float(@expense_sum) / Float(@budget_sum)) * 100)
+
       remain_amount = 0
       amount = 0
       expense = 0
@@ -48,6 +75,52 @@ class BudgetAndResultsManagementsController < ApplicationController
         end
         r[:remain_amount] = remain_amount
         r[:digestibility] = (amount == 0)?(0):((Float(expense) / Float(amount)) * 100)
+      end
+
+      #
+      if params[:output_tsv]
+        #data = Manifestation.get_manifestation_list_tsv(manifestations_for_output, current_user)
+        data = String.new
+        data << "\xEF\xBB\xBF".force_encoding("UTF-8") + "\n"
+
+        columns = [
+          [:id, 'activerecord.attributes.budget_and_results_managements.id'],
+          [:item_of_expenditure, 'activerecord.attributes.budget_and_results_managements.item_of_expenditure'],
+          [:expense, 'activerecord.attributes.budget_and_results_managements.expense'],
+          [:budget, 'activerecord.attributes.budget_and_results_managements.budget'],
+          [:amount, 'activerecord.attributes.budget_and_results_managements.remain_amount'],
+          [:digestibility, 'activerecord.attributes.budget_and_results_managements.digestibility']
+        ]
+
+        # title column
+        row = columns.map{|column| I18n.t(column[1])}
+        data << '"' + row.join("\"\t\"") +"\"\n"
+
+        #
+        cnt = 1
+        @results.each do |r|
+          row = []
+          columns.each do |column|
+            case column[0]
+            when :id
+              row << cnt.to_s
+            when :item_of_expenditure
+              row << r[:item_of_expenditure]
+            when :expense
+              row << r[:expense]
+            when :budget
+              row << r[:budget]
+            when :amount
+              row << r[:remain_amount]
+            when :digestibility
+              row << r[:digestibility]
+            end
+          end
+          data << '"' + row.join("\"\t\"") +"\"\n"
+        end
+
+        send_data data, :filename => "budget_and_results.tsv"
+        return 
       end
     end
   end
