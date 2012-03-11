8072a1df54e8dc7148089e8c876222c7588b9351
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-03-11 13:15:33 +0900

    refs 2686 カスタマバーコードの印字を追加

--- app/models/zip_code_list.rb
+++ app/models/zip_code_list.rb
@@ -1,7 +1,33 @@
+# -*- encoding: utf-8 -*-
 class ZipCodeList < ActiveRecord::Base
 
   searchable do
     text :prefecture_name, :city_name, :region_name
+  end
+
+  public
+  def check_include_address(address)
+    if self.flag10 && self.flag10 == "1"
+      self.region_name =~ /(.*)（(.*)丁目）/
+      unless $2 
+        logger.error "error"
+        raise ArgumentError, "trap 1-1"
+      end
+
+    else
+      asub = self.prefecture_name+self.city_name+self.region_name
+      logger.debug "asub1=#{asub}"
+      unless address.index(asub) == 0
+        logger.info "try matching city+region"
+        asub = self.city_name + self.region_name
+        logger.debug "asub2=#{asub}"
+        unless address.index(asub) == 0
+          raise ArgumentError, "trap 1-2"
+        end
+      end
+    end
+
+    return asub
   end
 
   def self.import(filename = nil)
@@ -9,24 +35,33 @@ class ZipCodeList < ActiveRecord::Base
     rows = zipcode_import.open_import_file(filename)
     ZipCodeList.delete_all
 
-    rows.each do |row|
-      h = {}
-      h[:union_code] = row[0]
-      h[:zipcode7] = row[2]
-      h[:prefectrure_name_kana] = row[3]
-      h[:city_name_kana] = row[4]
-      h[:region_name_kana] = row[5]
-      h[:prefecture_name] = row[6]
-      h[:city_name] = row[7]
-      h[:region_name] = row[8]
-      h[:flag10] = row[9]
-      h[:flag11] = row[10]
-      h[:flag12] = row[11]
-      h[:flag13] = row[12]
-      h[:flag14] = row[13]
-      h[:update_flag] = row[14]
-      ZipCodeList.create!(h)
-    end
+    #ZipCodeList.transaction do
+      rows.each_with_index do |row, row_num|
+        h = {}
+        h[:union_code] = row[0]
+        h[:zipcode7] = row[2]
+        h[:prefectrure_name_kana] = row[3]
+        h[:city_name_kana] = row[4]
+        h[:region_name_kana] = row[5]
+        h[:prefecture_name] = row[6]
+        h[:city_name] = row[7]
+        h[:region_name] = row[8]
+        h[:flag10] = row[9]
+        h[:flag11] = row[10]
+        h[:flag12] = row[11]
+        h[:flag13] = row[12]
+        h[:flag14] = row[13]
+        h[:update_flag] = row[14]
+        ZipCodeList.create!(h)
+
+        if row_num % 200 == 0
+          Sunspot.commit
+          GC.start
+          puts "#{row_num} success."
+        end
+      end
+    #end
+    Sunspot.commit
 
   end
 
