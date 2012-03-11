a4e58896efed0b2d10e62d3d2c0d9f0811313c78
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-03-12 02:43:54 +0900

    fixed importing creators

--- app/models/resource_import_file.rb
+++ app/models/resource_import_file.rb
@@ -484,7 +484,7 @@ class ResourceImportFile < ActiveRecord::Base
             creator_transcriptions = row['series_statement_creator_transcription'].to_s.split('//')
             creators_list = creators.zip(creator_transcriptions).map{|f,t| {:full_name => f.to_s.strip, :full_name_transcription => t.to_s.strip}}
             creator_patrons = Patron.import_patrons(creators_list)
-            series_statement.root_manifestation.creators = creator_patrons unless creator_patrons.empty?
+            series_statement.root_manifestation.creators = creator_patrons.uniq unless creator_patrons.empty?
           end
         end
       end
