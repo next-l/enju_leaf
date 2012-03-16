92cb4139b5136670e2f062cabbacf41a9fbebdd7
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-03-16 15:05:59 +0900

    refs 3226 modified rspec

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -9,6 +9,9 @@ describe Library do
   it "should should create default shelf" do
     @library.shelves.first.should be_true
     @library.shelves.first.name.should eq "#{@library.name}_default"
+
+    @library.shelves.second.should be_true
+    @library.shelves.second.open_access.should eq 9
   end
 end
 
