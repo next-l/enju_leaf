a08e56670ffc332467286dd40c48811535d0f3dd
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-04-06 17:22:15 +0900

    delayed_jobの実行時間を最大5分から12時間に変更

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,6 @@
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 5.minutes
+#Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 12.hours
 Delayed::Worker.backend = :active_record
 Delayed::Worker.delay_jobs = !Rails.env.test?
