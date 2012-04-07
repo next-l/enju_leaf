cccf6b234db52aa66cda61d398284b93eee555f3
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-04-07 21:10:05 +0900

    delayed_job中のログの保存先を変更

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,13 @@
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
-#Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.max_run_time = 12.hours
 Delayed::Worker.backend = :active_record
 Delayed::Worker.delay_jobs = !Rails.env.test?
+
+Delayed::Worker.logger = 
+  ActiveSupport::BufferedLogger.new("log/#{Rails.env}_delayed_jobs.log", Rails.logger.level)
+Delayed::Worker.logger.auto_flushing = 1
+if caller.last =~ /.*\/script\/delayed_job:\d+$/
+  ActiveRecord::Base.logger = Delayed::Worker.logger
+end
+
