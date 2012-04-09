24601e630c594351425c296f1aff4fca042f178a
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-04-09 18:32:05 +0900

    delayed_job ログ出力設定の変更

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -6,8 +6,7 @@ Delayed::Worker.delay_jobs = !Rails.env.test?
 
 Delayed::Worker.logger = 
   ActiveSupport::BufferedLogger.new("log/#{Rails.env}_delayed_jobs.log", Rails.logger.level)
-Delayed::Worker.logger.auto_flushing = 1
-if caller.last =~ /.*\/script\/delayed_job:\d+$/
-  ActiveRecord::Base.logger = Delayed::Worker.logger
-end
+#if caller.last =~ /.*\/script\/delayed_job:\d+$/
+#  ActiveRecord::Base.logger = Delayed::Worker.logger
+#end
 
