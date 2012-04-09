3e2ab043aa79dd0a4365098409b5035e2b32f592
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-04-09 18:30:56 +0900

    refs 3469 メッセージフィルタを追加

--- app/controllers/resource_import_results_controller.rb
+++ app/controllers/resource_import_results_controller.rb
@@ -7,7 +7,13 @@ class ResourceImportResultsController < InheritedResources::Base
 
   def index
     @resource_import_file = ResourceImportFile.where(:id => params[:resource_import_file_id]).first
-    @resource_import_results = @resource_import_file.resource_import_results if @resource_import_file
+    if @resource_import_file
+      if params[:display_result] && params[:display_result] == "has_msg"
+        @resource_import_results = @resource_import_file.resource_import_results.where("error_msg is not null")
+      else
+        @resource_import_results = @resource_import_file.resource_import_results 
+      end
+    end
     @results_num = @resource_import_results.length
     @resource_import_results = @resource_import_results.page(params[:page]) unless params[:format] == 'tsv'
 
