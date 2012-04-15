4784af0991bdb0ebcd4fde766f1c47f53286b0eb
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-04-16 05:40:08 +0900

    fixed typo

--- db/migrate/20120415060323_rename_patron_import_file_imported_at_to_executed_at.rb
+++ db/migrate/20120415060323_rename_patron_import_file_imported_at_to_executed_at.rb
@@ -4,6 +4,6 @@ class RenamePatronImportFileImportedAtToExecutedAt < ActiveRecord::Migration
   end
 
   def down
-    rename_column :patron_import_files, :executed_at, :impored_at
+    rename_column :patron_import_files, :executed_at, :imported_at
   end
 end
