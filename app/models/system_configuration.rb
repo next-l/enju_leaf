d7522948b1603d6ae49826fc4e4006e8eb3a2e31
Akifumi NAKAMURA <nakamura@opentech.co.jp>
2012-04-18 18:38:29 +0900

    refs 3212 キャッシュ機能を追加した

--- app/models/system_configuration.rb
+++ app/models/system_configuration.rb
@@ -1,6 +1,10 @@
 class SystemConfiguration < ActiveRecord::Base
   default_scope order('id ASC') 
 
+  after_commit lambda {    
+    Rails.cache.clear
+  }
+
   def self.typenames
     ["String","Boolean","Numeric"]
   end
@@ -9,9 +13,27 @@ class SystemConfiguration < ActiveRecord::Base
   validates_inclusion_of :typename, :in => SystemConfiguration.typenames
   validate :value_by_typename_is_valid
 
+  Prefix_Key = "systemconfig_"
+
   def self.get(keyname)
-    value = SystemConfiguration.where(:keyname => keyname).first.v rescue nil
-    typename = SystemConfiguration.where(:keyname => keyname).first.typename rescue nil
+    value = typename = nil
+
+    s = Rails.cache.read("#{Prefix_Key}#{keyname}")
+    if s
+      logger.debug "using cache:#{Prefix_Key}#{keyname}"
+    else
+      s = SystemConfiguration.where(:keyname => keyname).first rescue nil
+      if s
+        Rails.cache.write("#{Prefix_Key}#{keyname}", s)
+      end
+    end
+    if s
+      value = s.v
+      typename = s.typename
+    end
+
+    #value = SystemConfiguration.where(:keyname => keyname).first.v rescue nil
+    #typename = SystemConfiguration.where(:keyname => keyname).first.typename rescue nil
     if value and typename
       case typename
       when "String"
@@ -32,12 +54,10 @@ class SystemConfiguration < ActiveRecord::Base
 
   private  
   def value_by_typename_is_valid
-    logger.info "aaaa"
     unless self.v.blank?
       case typename
       when "String"
       when "Boolean"
-        logger.info "bbb"
         unless ["true","false"].include?(v)
           errors.add(:v, I18n.t('activerecord.attributes.system_configuration.invalid_format')) 
         end
