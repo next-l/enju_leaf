7410a628f2aad3b1f4b81a64e05b0d4d354cfcf4
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-30 05:39:08 +0900

    moved some methods

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -17,6 +17,17 @@ class UserGroup < ActiveRecord::Base
   def self.per_page
     10
   end
+
+  if defined?(EnjuCirculation)
+    has_many :user_group_has_checkout_types, :dependent => :destroy
+    has_many :checkout_types, :through => :user_group_has_checkout_types, :order => :position
+    has_many :lending_policies
+
+    validates_numericality_of :number_of_day_to_notify_due_date,
+      :number_of_day_to_notify_overdue,
+      :number_of_time_to_notify_overdue,
+      :greater_than_or_equal_to => 0
+  end
 end
 
 # == Schema Information
