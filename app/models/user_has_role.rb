d911c9de14b2e7b930c1277b60c1bcf1c4769759
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-03 05:04:00 +0900

    updated accessible attributes

--- app/models/user_has_role.rb
+++ app/models/user_has_role.rb
@@ -1,6 +1,5 @@
 class UserHasRole < ActiveRecord::Base
-  attr_accessible
-  attr_accessible :user_id, :role_id, :as => :admin
+  attr_accessible :user_id, :role_id
   belongs_to :user
   belongs_to :role
   accepts_nested_attributes_for :role
