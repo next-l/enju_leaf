1ada3aa2a27687faac5fb344d5e8f1e0bf166643
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-22 06:29:44 +0900

    some assets were not loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,12 @@
 //= require jquery.highlight
 //= require jquery.shortcut
 //= require jquery.cookie
+//= require fg.menu
+//= require fg.menu.enju
 //= require pagination
 //= require select_locale
+//= require portlets
+//= require tab_view
 //= require event_calendar
 //= require rails.validations
 //= require jquery.qtip
