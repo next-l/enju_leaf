ac8f7847eeb0ee80737fa7870eb1380ebdece225
reiko <reiko@enjudev1.(none)>
2012-08-06 16:54:23 +0900

    delete file of event_categoriess

--- app/helpers/event_categories_helper.rb
+++
@@ -1,6 +0,0 @@
-module EventCategoriesHelper
-  def select_checkin_date
-    [[t('activerecord.attributes.event_category.checkin_ng_post'), 1], [t('activerecord.attributes.event_category.checkin_ng_pre'), 2]]
-  end
-end
-
