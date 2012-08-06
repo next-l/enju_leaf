1959de00b44c655473a579f93ce51bc08a09772a
reiko <reiko@enjudev1.(none)>
2012-08-06 19:14:27 +0900

    delete file of event_import_results

--- db/migrate/20120806093754_create_event_import_results.enju_event_engine.rb
+++ db/migrate/20120806093754_create_event_import_results.enju_event_engine.rb
@@ -1,9 +1,11 @@
+# This migration comes from enju_event_engine (originally 20100925074639)
 class CreateEventImportResults < ActiveRecord::Migration
   def self.up
     create_table :event_import_results do |t|
       t.integer :event_import_file_id
       t.integer :event_id
       t.text :body
+      t.string :error_msg
 
       t.timestamps
     end
