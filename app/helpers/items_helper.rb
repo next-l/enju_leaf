8b3733e5378d7109f70dbb7a01f6202593ba2d71
shinozuka <reiko@opentech.co.jp>
2012-08-21 16:47:20 +0900

    modified call_numberformat

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,8 +3,10 @@ module ItemsHelper
     call_number = item.call_number
     if SystemConfiguration.get("items.call_number.delete_first_delimiter")
       delimiter = item.shelf.library.call_number_delimiter 
-      if call_number.slice(0, 1) == delimiter
-        call_number.slice!(0, 1)
+      if call_number
+        if call_number.slice(0, 1) == delimiter
+          call_number.slice!(0, 1)
+        end
       end
     end   
     return call_number
