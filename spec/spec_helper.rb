b74a64105f64cf29ebe977df17b3e49e6fcb3782
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-09-22 22:33:14 +0900

    モデルを修正しても spork 側にそれが反映されないのを修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,6 +65,11 @@ end
 Spork.each_run do
   # This code will be run each time you run your specs.
 
+  silence_warnings do
+    Dir[Rails.root.join('app/**/*.rb')].each do |file|
+      load file
+    end
+  end
 end
 
 # --- Instructions ---
