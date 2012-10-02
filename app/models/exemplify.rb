367b04e97729da2f5b12b37fc6241824a4e03d82
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-10-02 14:16:54 +0900

    refs 3984 人物のインポートが動いていなかったので修正。attr_accessibleを追加

---
+++ app/models/exemplify.rb
@@ -0,0 +1,8 @@
+require EnjuTrunkFrbr::Engine.root.join('app', 'models', 'exemplify')
+class Exemplify < ActiveRecord::Base
+  attr_accessible :manifestation_id, :item_id, :position
+  self.extend ItemsHelper
+
+end
+
+
