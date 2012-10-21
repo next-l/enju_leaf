50232eefa9ec7ad291c57ef72cb0a45e749eb264
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-10-21 18:00:26 +0900

    refs 4034 棚に画像を追加するとエラーになる問題を解消(attr_accessibleを追加)

--- app/models/picture_file.rb
+++ app/models/picture_file.rb
@@ -1,4 +1,6 @@
 class PictureFile < ActiveRecord::Base
+  attr_accessible :picture_attachable_id, :picture_attachable_type, :picture
+
   scope :attached, where('picture_attachable_id > 0')
   belongs_to :picture_attachable, :polymorphic => true, :validate => true
 
