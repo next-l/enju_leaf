d763c923ab9d31158e80f7e42f35484c317640d5
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-10-21 20:49:37 +0900

    refs 4012 連番桁数の設定を可能になるように修正

--- app/models/barcode_list.rb
+++ app/models/barcode_list.rb
@@ -1,5 +1,6 @@
 class BarcodeList < ActiveRecord::Base
-  attr_accessible :barcode_name, :sheet_id, :usage_type, :barcode_type, :barcode_prefix, :barcode_suffix, :label_note
+  attr_accessible :barcode_name, :sheet_id, :usage_type, :barcode_type, :barcode_prefix,
+                  :barcode_suffix, :label_note, :printed_number
 
   default_scope :order => 'barcode_prefix'
   BARCODE_TYPES = ["Code128B","NW-7"]
@@ -8,6 +9,8 @@ class BarcodeList < ActiveRecord::Base
   attr_accessor :start_number, :end_number
 
   belongs_to :sheet
+
+  RGhost::Config::GS[:path]='/usr/local/bin/gs' 
 
   def create_pdf_user_number_list(start_number, end_number)
     dir_base = "#{Rails.root}/private/system/barcode_list/#{self.id}/original/"
@@ -84,15 +87,17 @@ class BarcodeList < ActiveRecord::Base
   def create_pdf_sheet(start_number,end_number)
       dir_base = "#{Rails.root}/private/system/barcode_list/#{self.id}/original/"
       FileUtils.mkdir_p(dir_base) unless FileTest.exist?(dir_base)
-      filename = "barcode.pdf"
+      filename = "barcode#{Time.now.strftime('%s')}.pdf"
       File.delete(dir_base+filename) if File.exist?(dir_base+filename)
 
       barcode_sheet = self.sheet
       digit_number = 8
+      digit_number = self.printed_number unless self.printed_number.blank?
       type = self.barcode_type unless self.barcode_type.blank?
       prefix = self.barcode_prefix unless self.barcode_prefix.blank?
       suffix = self.barcode_suffix unless self.barcode_suffix.blank?
       digit_number = self.printed_number unless self.printed_number.blank?
+
       @code_words = []
       num_barcode = end_number.to_i - start_number.to_i
       num_barcode.times do |i|
@@ -100,6 +105,9 @@ class BarcodeList < ActiveRecord::Base
         #@code_words << prefix + sprintf('%0'+digit_number.to_s+'d',digit.to_s) + suffix
         @code_words << sprintf('%0'+digit_number.to_s+'d',digit.to_s) 
       end
+
+puts @code_words
+
       sheet = BarcodeSheet.new
       sheet.path = dir_base
       sheet.code_type = type
@@ -108,6 +116,7 @@ class BarcodeList < ActiveRecord::Base
     rescue => exc
       logger.error("barcode pdf sheet create failed: " + dir_base + filename)
       logger.info(exc)
+      logger.info($@.join("\n"))
       return
   end
 end
