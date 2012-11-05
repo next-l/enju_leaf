317f7f1e0323d179d8aaf4266a5ebf8292581702
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-11-05 18:58:24 +0900

    refs 4007 使っていないメソッドが残っていたので削除

--- app/controllers/opac_controller.rb
+++ app/controllers/opac_controller.rb
@@ -1,6 +1,5 @@
 class OpacController < ApplicationController
   layout "opac"
-  #include Devise::Controllers::InternalHelpers
 
   def index
     @events = Event.order('start_at DESC').limit(5)
@@ -12,9 +11,4 @@ class OpacController < ApplicationController
   def manifestations_index
   end
 
-  def signed_in
-    logger.info "OpacController login"
-    puts "@@@@v@"
-    puts @current_user
-  end
 end
