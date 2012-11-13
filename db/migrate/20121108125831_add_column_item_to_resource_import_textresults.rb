599512ec26aed3c83cabcadf02b2c7243af9348c
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2012-11-13 17:11:37 +0900

    migrateファイルが不正

--- db/migrate/20121108125831_add_column_item_to_resource_import_textresults.rb
+++ db/migrate/20121108125831_add_column_item_to_resource_import_textresults.rb
@@ -1,6 +1,6 @@
 class AddColumnItemToResourceImportTextresults < ActiveRecord::Migration
   def change
-    add_column :resource_import_textresults, :manifestation_id, :integer
+    #add_column :resource_import_textresults, :manifestation_id, :integer
   end
 
   add_index :resource_import_textresults, :manifestation_id
