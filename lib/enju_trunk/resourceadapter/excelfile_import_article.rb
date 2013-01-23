2fbb4ddd10e6f375fee329e8a210fbd00786a990
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-01-23 14:43:40 +0900

    refs 4085 文献のエクセルファイルでの編集機能を実装

--- lib/enju_trunk/resourceadapter/excelfile_import_article.rb
+++ lib/enju_trunk/resourceadapter/excelfile_import_article.rb
@@ -1,68 +1,76 @@
 # -*- encoding: utf-8 -*-
 module EnjuTrunk
   module ExcelfileImportArticle
-    def import_article(filename, id, extraparams)
-      manifestation_type = ManifestationType.find(eval(extraparams['manifestation_type']).to_i)
 
-      oo = Excelx.new(filename)
+    def import_article(filename, id, extraparams)
+      @manifestation_type = ManifestationType.find(extraparams['manifestation_type'].to_i)
+      @resource_import_textfile_id = id
+      @oo = Excelx.new(filename)
+
       extraparams["sheet"].each_with_index do |sheet, i|
         logger.info "num=#{i}  sheet=#{sheet}"
-        oo.default_sheet = sheet
-        import_article_start(oo, id, manifestation_type, sheet)
+        @oo.default_sheet = sheet
+        set_field(sheet)
+        import_article_data(sheet)
       end
     end 
 
-    def import_article_start(oo, resource_import_textfile_id, manifestation_type, sheet)
-      num = { :manifestation_imported => 0, :item_imported => 0, :manifestation_found => 0, :item_found => 0, :failed => 0 }
-      # setting read_area
+    def set_field(sheet)
       field_row_num = 1
-      first_row_num = 1
-      header_row_num = 1
-      first_column_num = 1
-      # set header field
-      field = Hash.new
-      first_column_num.upto(oo.last_column) do |column|
-        field.store(oo.cell(field_row_num, column).to_s, column)
-      end
-      # check head
-      require_head_article = [field[I18n.t('resource_import_textfile.excel.article.original_title')]]
-      if require_head_article.reject{|field| field.to_s.strip == ""}.empty?
-        import_textresult = ResourceImportTextresult.create!(:resource_import_textfile_id => resource_import_textfile_id, :body => '', :extraparams => "{'sheet'=>'#{sheet}'}")
-        import_textresult.error_msg = I18n.t('resource_import_textfile.error.article.head_is_blank', :sheet => oo.default_sheet)
-        import_textresult.save
-        raise
-      end
 
-      first_row_num.upto(oo.last_row) do |row|
+      # read the field, then set field a hash
+      @field = Hash::new
+      @oo.first_column.upto(@oo.last_column) do |column|
+        @field.store(@oo.cell(field_row_num, column).to_s, column)
+      end
+      # check that exist need fields
+      require_field = [@field[I18n.t('resource_import_textfile.excel.article.original_title')]]
+      import_textresult = ResourceImportTextresult.new(
+        :resource_import_textfile_id => @resource_import_textfile_id,
+        :extraparams                 => "{'sheet'=>'#{sheet}'}",
+      )
+      if require_field.reject{|field| field.to_s.strip == ""}.empty?
+        import_textresult.error_msg = I18n.t('resource_import_textfile.error.article.head_is_blank', :sheet => sheet)
+      else
+        import_textresult.body      = @field.keys.join("\t")
+        import_textresult.error_msg = I18n.t('resource_import_textfile.message.read_sheet', :sheet => sheet)
+      end
+      import_textresult.save
+
+      raise if import_textresult.body.nil?
+    end
+
+    def import_article_data(sheet)
+      first_data_row_num = 2
+      num = { :manifestation_imported => 0, :item_imported => 0, :manifestation_found => 0, :item_found => 0, :failed => 0 }
+
+      first_data_row_num.upto(@oo.last_row) do |row|
         Rails.logger.info("import block start. row_num=#{row}")
-        body = []
-        first_column_num.upto(oo.last_column) do |column|
-          body << oo.cell(row, column)
-        end
-        import_textresult = ResourceImportTextresult.create!(:resource_import_textfile_id => resource_import_textfile_id, :body => body.join("\t"), :extraparams => "{'sheet'=>'#{sheet}'}")
-        if row.to_i == header_row_num.to_i
-          import_textresult.error_msg = 'HEADER DATA'
-          import_textresult.save!
-          next
-        end
 
+        datas = Hash::new
+        @oo.first_column.upto(@oo.last_column) do |column|
+           datas.store(column, fix_data(@oo.cell(row, column).to_s.strip))
+        end
+        import_textresult = ResourceImportTextresult.new(
+          :resource_import_textfile_id => @resource_import_textfile_id,
+          :body                        => datas.values.join("\t"),
+          :extraparams                 => "{'sheet'=>'#{sheet}'}"
+        )
         # check cell
-        require_cell_article = [oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.original_title')]).to_s.strip]
-        if require_cell_article.reject{|field| field.to_s.strip == ""}.empty?
-          import_textresult.error_msg = "FAIL[sheet:#{oo.default_sheet} row:#{row}] #{I18n.t('resource_import_textfile.error.article.cell_is_blank')}"
+        require_cell = [datas[@field[I18n.t('resource_import_textfile.excel.article.original_title')]]]
+        if require_cell.reject{ |field| field.to_s.strip == "" }.empty?
+          import_textresult.error_msg = "FAIL[sheet:#{sheet} row:#{row}] #{I18n.t('resource_import_textfile.error.article.cell_is_blank')}"
           import_textresult.save
           next
         end
 
         begin
-          exist_same_article?(oo, row, field, manifestation_type)
-          manifestation = fetch_article(oo, row, field, manifestation_type)
+          manifestation = fetch_article(datas)
           num[:manifestation_imported] += 1 if manifestation
           import_textresult.manifestation = manifestation
-
           if manifestation.valid?
-            resource_import_textfile = ResourceImportTextfile.find(resource_import_textfile_id)
-            import_textresult.item = create_article_item(oo, row, field, manifestation, resource_import_textfile)
+            resource_import_textfile = ResourceImportTextfile.find(@resource_import_textfile_id)
+            import_textresult.item = create_article_item(datas, manifestation, resource_import_textfile)
             manifestation.index
             num[:item_imported] +=1 if import_textresult.item
 
@@ -77,12 +85,11 @@ module EnjuTrunk
             num[:failed] += 1
           end
         rescue => e
-          import_textresult.error_msg = "FAIL[sheet:#{oo.default_sheet} row:#{row}]: #{e.message}"
-          Rails.logger.info("FAIL[sheet:#{oo.default_sheet} row:#{row}] resource registration failed: column #{row}: #{e.message}")
-          Rails.logger.info("FAIL[sheet:#{oo.default_sheet} row:#{row}] #{$@}")
+          import_textresult.error_msg = "FAIL[sheet:#{sheet} row:#{row}]: #{e.message}"
+          Rails.logger.info("FAIL[sheet:#{sheet} row:#{row}] resource registration failed: column #{row}: #{e.message}")
+          Rails.logger.info("FAIL[sheet:#{sheet} row:#{row}] #{$@}")
           num[:failed] += 1
         end
-
         import_textresult.save!
 
         if row % 50 == 0
@@ -95,65 +102,48 @@ module EnjuTrunk
       return num
     end
 
-    def exist_same_article?(oo, row, field, manifestation_type)
-      # check field
-      original_title = fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.original_title')]).to_s.strip).to_s
-      article_title  = fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.title')]).to_s.strip).to_s
-      creators       = set_article_creatos(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.creator')]).to_s.strip, manifestation_type)     
-
-      article = Manifestation.find(
-        :first,
-        :include => [:creators, :subjects],
-        :conditions => {
-          :original_title => original_title,
-          :article_title  => article_title,
-        }
-      ) rescue nil
-      if article
-        if article.creators.map{ |c| c.full_name }.sort == creators.sort
-          raise I18n.t('resource_import_textfile.error.article.exist_same_article')
-        end
-      end
-    end
-
-    def fetch_article(oo, row, field, manifestation_type)
-      manifestation = nil
-
-      # 更新、削除はどうする？
-      title = {}
-      title[:original_title] = fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.original_title')]).to_s.strip)
-      title[:article_title]  = fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.title')]).to_s.strip)
-      start_page, end_page   = set_page(fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.number_of_page')]).to_s.strip))
-      number_field = field[I18n.t('resource_import_textfile.excel.article.volume_number_string')]
-      volume_number_string, issue_number_string = set_number(fix_data(oo.cell(row, number_field).to_s.strip), manifestation_type)
-
+    def fetch_article(datas)
       ResourceImportTextfile.transaction do
         begin
-          manifestation                      = Manifestation.new(title)
-          manifestation.pub_date             = fix_data(oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.pub_date')]).to_s.strip)
-          manifestation.volume_number_string = volume_number_string
-          manifestation.issue_number_string  = issue_number_string
+          @mode = 'create'
+          manifestation = exist_same_article?(datas)
+
+          unless manifestation.nil?
+            @mode = 'edit'
+          else       
+            manifestation = Manifestation.new(
+              :original_title => datas[@field[I18n.t('resource_import_textfile.excel.article.original_title')]],
+              :article_title  => datas[@field[I18n.t('resource_import_textfile.excel.article.title')]],
+              :frequency      => Frequency.where(:name => 'unknown').first,
+              :required_role  => Role.find('Guest'),
+              :during_import  => true,
+            )
+          end
+
+          start_page, end_page        = set_page(datas[@field[I18n.t('resource_import_textfile.excel.article.number_of_page')]])
+          volume_number, issue_number = set_number(datas[@field[I18n.t('resource_import_textfile.excel.article.volume_number_string')]])
+
+          manifestation.pub_date             = datas[@field[I18n.t('resource_import_textfile.excel.article.pub_date')]]
+          manifestation.volume_number_string = volume_number
+          manifestation.issue_number_string  = issue_number
           manifestation.start_page           = start_page
           manifestation.end_page             = end_page
-          manifestation.manifestation_type   = manifestation_type
-          manifestation.frequency            = Frequency.where(:name => 'unknown').first
-          manifestation.required_role        = Role.find('Guest')
-          manifestation.during_import        = true
+          manifestation.manifestation_type   = @manifestation_type
           manifestation.save!
 
-          creators_cell = oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.creator')]).to_s.strip
-          creators = set_article_creatos(creators_cell, manifestation_type)
-          creators_list = creators.inject([]){ |list, creator| list << {:full_name => creator.to_s.strip, :full_name_transcription => "" } }
-          creator_patrons = Patron.import_patrons(creators_list)
-          manifestation.creators << creator_patrons
+          if @mode == 'create'
+            creators_cell   = datas[@field[I18n.t('resource_import_textfile.excel.article.creator')]]
+            creators        = set_article_creatos(creators_cell)
+            creators_list   = creators.inject([]){ |list, creator| list << {:full_name => creator.to_s.strip, :full_name_transcription => "" } }
+            creator_patrons = Patron.import_patrons(creators_list)
+            manifestation.creators = creator_patrons
+          end
 
-          subjects_cell = oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.subject')]).to_s.strip
-          subjects_list = set_article_subjects(subjects_cell, manifestation_type)
-          subjects = Subject.import_subjects (subjects_list)
-          manifestation.subjects << subjects
+          subjects_cell = datas[@field[I18n.t('resource_import_textfile.excel.article.subject')]]
+          subjects_list = set_article_subjects(subjects_cell)
+          subjects      = Subject.import_subjects (subjects_list)
+          manifestation.subjects = subjects
 
-          #TODO: manifestationをsaveする前にcreateが先に呼ばれてしまうので一旦manifestationの作成を行ってから追加する
-          manifestation.save!
           return manifestation
         rescue Exception => e
           p "error at fetch_new: #{e.message}"
@@ -162,7 +152,48 @@ module EnjuTrunk
       end
     end
 
-    def set_number(num, manifestation_type)
+    def exist_same_article?(datas)
+      original_title = datas[@field[I18n.t('resource_import_textfile.excel.article.original_title')]]
+      article_title  = datas[@field[I18n.t('resource_import_textfile.excel.article.title')]]
+      creators       = set_article_creatos(datas[@field[I18n.t('resource_import_textfile.excel.article.creator')]])
+
+      article =  Manifestation.find(
+        :first,
+        :include => [:creators],
+        :conditions => "original_title = \'#{original_title}\' and article_title = \'#{article_title}\' and creates.id is not null"
+      )
+
+      if article
+        if article.creators.map{ |c| c.full_name }.sort == creators.sort
+          return article
+        else
+          return nil
+        end
+      end
+    end
+
+    def create_article_item(datas, manifestation, resource_import_textfile)
+      item = nil
+      if @mode == 'create' or manifestation.items.nil?
+        item = Item.new(
+          :item_identifier    => Numbering.do_numbering('article'),
+          :shelf              => resource_import_textfile.user.library.article_shelf,
+          :circulation_status => CirculationStatus.where(:name => 'Not Available').first,
+          :use_restriction_id => UseRestriction.where(:name => 'Not For Loan').first,
+          :rank               => 0,
+          :manifestation      => manifestation
+        )
+      else
+        item = manifestation.items.first
+      end
+      item.call_number        = datas[@field[I18n.t('resource_import_textfile.excel.article.call_number')]]
+      item.url                = datas[@field[I18n.t('resource_import_textfile.excel.article.url')]]
+      item.save!
+      item.patrons << resource_import_textfile.user.library.patron if @mode == 'create' or manifestation.items.nil?
+      return item
+    end
+
+    def set_number(num)
       unless num.match(/\*/)
         return '', num 
       else
@@ -179,35 +210,22 @@ module EnjuTrunk
         unless page.match(/\-/)
           start_page, end_page = page, page
         else
-        start_page, end_page = page.split('-')[0], page.split('-')[1]
+          start_page, end_page = page.split('-')[0], page.split('-')[1]
         end
       end
       return start_page, end_page
     end
 
-    def set_article_creatos(cell, manifestation_type)
+    def set_article_creatos(cell)
       creators = []
-      creators = manifestation_type.name == 'japanese_article' ? cell.split(';') : cell.split(' ')
+      creators = @manifestation_type.name == 'japanese_article' ? cell.split(';') : cell.split(' ')
       return creators
     end
 
-    def set_article_subjects(cell, manifestation_type)
+    def set_article_subjects(cell)
       subjects = []
-      subjects = manifestation_type.name == 'japanese_article' ? cell.split(';') : cell.split('*')
+      subjects = @manifestation_type.name == 'japanese_article' ? cell.split(';') : cell.split('*')
       return subjects
-    end
-
-    def create_article_item(oo, row, field, manifestation, resource_import_textfile)
-      item = import_item(manifestation, {
-        :call_number        => oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.call_number')]).to_s.strip,
-        :url                => oo.cell(row, field[I18n.t('resource_import_textfile.excel.article.url')]).to_s.strip,
-        :item_identifier    => Numbering.do_numbering('article'),
-        :shelf              => resource_import_textfile.user.library.article_shelf,
-        :circulation_status => CirculationStatus.where(:name => 'Not Available').first,
-        :use_restriction_id => UseRestriction.where(:name => 'Not For Loan').first,
-        :rank               => 0,
-      })
-      return item
     end
   end
 end
