961db73a13e3dd366ca200e2a40084c18b32b8f4
Emiko TAMIYA <emiko.tamiya@gmail.com>
2013-02-01 18:42:36 +0900

    refs 4422 statistic report for gov. library

--- app/models/manifestation_type.rb
+++ app/models/manifestation_type.rb
@@ -3,9 +3,15 @@ class ManifestationType < ActiveRecord::Base
   attr_accessible :display_name, :name, :note, :position
   default_scope :order => "position"
   has_many :manifestations
+  has_many :statistics if defined?(Statistic)
+  scope :book, where(["name = ? OR name =? OR name like ?", 'japanese_book', 'foreign_book', '%monograph'])
+  scope :series, where(["name like ? OR name like ?", '%magazine', '%serial_book'])
+  scope :article, where(["name like ?", '%article'])
+  scope :exinfo, where(["name like ? OR name like ?", 'unkown', 'exinfo%'])
 
   def is_book?
-    if ['japanese_book', 'foreign_book'].include?(self.name)
+    if ['japanese_book', 'foreign_book',
+        'japanese_monograph', 'foreign_monograph'].include?(self.name)
       return true
     end
     false
@@ -20,17 +26,25 @@ class ManifestationType < ActiveRecord::Base
 
   def is_series?
     if ['japanese_magazine', 'foreign_magazine',
-      'japanese_serial_book', 'foreign_serial_book',
-      'japanese_monograph', 'foreign_monograph'].include?(self.name)
+      'japanese_serial_book', 'foreign_serial_book'].include?(self.name)
       return true
     end
     false
   end
 
   def is_exinfo?
-    if ['exinfo1', 'exinfo2', 'exinfo3'].include?(self.name)
+    if ['unknown', 'exinfo1', 'exinfo2', 'exinfo3'].include?(self.name)
       return true
     end
     false
   end
+
+  def self.categories
+    ['book', 'series', 'article', 'exinfo']
+  end
+  
+  def self.type_ids(name)
+    eval("ManifestationType.#{name}.collect(&:id)")    
+  end
+
 end
