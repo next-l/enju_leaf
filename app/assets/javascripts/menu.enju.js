819ab2067d29e9065a543a52cebe0d2966cfe6e3
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-02-12 14:51:37 -0600

    cleaned up

--- app/assets/javascripts/menu.enju.js
+++ app/assets/javascripts/menu.enju.js
@@ -1,10 +1,4 @@
 $(function(){
-  function select(event, ui) {
-    $("<div/>").text("Selected: " + ui.item.text()).appendTo("#log");
-    if (ui.item.text() == 'Quit') {
-      $(this).menubar('destroy');
-    }
-  }
   $('#bar1').menubar({
     position:{
       within: $("#demo-frame").add(window).first()
