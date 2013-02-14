ebad91df157caddf0dddd9a68d46b95849de9cf1
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-02-14 20:50:08 +0900

    refs4085 subjectモデルを追加

---
+++ app/models/subject.rb
@@ -0,0 +1,20 @@
+require EnjuSubject::Engine.root.join('app', 'models', 'subject')
+class Subject < ActiveRecord::Base
+  def self.import_subjects(subject_lists)
+    list = []
+    subject_lists.each do |s|
+      subject = Subject.where(:term => s.to_s.strip).first
+      unless subject
+        # TODO: Subject typeの設定
+        subject = Subject.new(
+          :term => s.to_s.strip,
+          :subject_type_id => 1,
+        )
+        subject.required_role = Role.where(:name => 'Guest').first
+        subject.save
+      end
+      list << subject
+    end
+    list
+  end
+end
