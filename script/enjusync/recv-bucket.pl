b7a86dc4952bb706c74150ee97a75c803b742f34
Akifumi NAKAMURA <nakamura.akifumi@miraitsystems.jp>
2013-02-22 21:07:43 +0900

    Changed logic of calling rake command. Ignore rake exit status.

--- script/enjusync/recv-bucket.pl
+++ script/enjusync/recv-bucket.pl
@@ -152,10 +152,14 @@ unless (chdir "$ProgHome") {
 # enju_trunkで実行しないといけない
 if ($debug) {print "rake enju:sync:import DUMP_FILE=$RecvDir/$rcv_bucket/enjudump.marshal STATUS_FILE=$RecvDir/$rcv_bucket/status.marshal\n";}
 #
-unless (system("rake enju:sync:import DUMP_FILE=$RecvDir/$rcv_bucket/enjudump.marshal STATUS_FILE=$RecvDir/$rcv_bucket/status.marshal")) {
-	wrt_log($pname, 'err', "Failed import dumpfile: see also /opt/enju_trunk/logs/production.log");
-	close(LCK);
-	exit 2;
+# unless (system("rake enju:sync:import DUMP_FILE=$RecvDir/$rcv_bucket/enjudump.marshal STATUS_FILE=$RecvDir/$rcv_bucket/status.marshal")) {
+# 	wrt_log($pname, 'err', "Failed import dumpfile: see also /opt/enju_trunk/log/production.log");
+# 	close(LCK);
+# 	exit 2;
+# }
+my $r_stat = system("rake enju:sync:import DUMP_FILE=$RecvDir/$rcv_bucket/enjudump.marshal STATUS_FILE=$RecvDir/$rcv_bucket/status.marshal");
+if ( $r_stat != 0 ) {
+ 	wrt_log($pname, 'err', "Failed import dumpfile:status=$r_stat:see also /opt/enju_trunk/log/production.log");
 }
 
 # 受信バケットに再移動
