115190c120287376ff4d656ee14dc567eeca0fcb
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-02-23 23:27:42 +0900

    refs 4679 migrationファイルの修正(type,sqlite)

--- db/migrate/20130116043057_add_user_status_id.rb
+++ db/migrate/20130116043057_add_user_status_id.rb
@@ -1,7 +1,7 @@
 class AddUserStatusId < ActiveRecord::Migration
   def up
     add_column :users, :user_status_id, :integer, :default => 1, :null => false
-    add_column :user_statuses, :state_id, :integer, :defaut => 1, :null => false
+    add_column :user_statuses, :state_id, :integer, :default => 1, :null => false
   end
 
   def down
