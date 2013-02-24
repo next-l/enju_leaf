f782c5681d8a5277cd3d51ac1ac39efadf40c8be
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-02-25 00:42:30 +0900

    バーコード出力修正

--- script/tools/function_barcode_output.rb
+++ script/tools/function_barcode_output.rb
@@ -1,51 +1,20 @@
 require 'barby'
 require 'barby/barcode/code_128'
-require 'barby/barcode/code_128'
 require 'barby/outputter/ascii_outputter'
 require 'barby/outputter/rmagick_outputter'
 require 'barby/outputter/png_outputter'
 
-=begin
-barcode = Barby::Code128A.new("ABC123")
-#puts barcode.to_ascii
-File.open("barby_test1.jpg", "wb") do |f|
-  f << barcode.to_jpg
+def generate_barcode(s, file_prefix)
+  data = s.force_encoding("BINARY")
+  barcode = Barby::Code128A.new(data)
+  File.open("barby_function_#{file_prefix}.jpg", "wb") do |f|
+    f << barcode.to_jpg
+  end
 end
 
-data = ("ABC123\306def\3074567").force_encoding("BINARY")
-barcode = Barby::Code128A.new(data)
-#puts barcode.to_ascii
-File.open("barby_test2.jpg", "wb") do |f|
-  f << barcode.to_jpg
-end
-=end
-
-data = ("DUMMY\x12").force_encoding("BINARY")
-barcode = Barby::Code128A.new(data)
-#puts barcode.to_ascii
-File.open("barby_test_f2.jpg", "wb") do |f|
-  f << barcode.to_jpg
-end
-
-data = ("DUMMY\x13").force_encoding("BINARY")
-barcode = Barby::Code128A.new(data)
-#puts barcode.to_ascii
-File.open("barby_test_f3.jpg", "wb") do |f|
-  f << barcode.to_jpg
-end
-
-data = ("DUMMY\x14").force_encoding("BINARY")
-barcode = Barby::Code128A.new(data)
-#puts barcode.to_ascii
-File.open("barby_test_f4.jpg", "wb") do |f|
-  f << barcode.to_jpg
-end
-
-data = ("DUMMY\x16").force_encoding("BINARY")
-barcode = Barby::Code128A.new(data)
-#puts barcode.to_ascii
-File.open("barby_test_f6.jpg", "wb") do |f|
-  f << barcode.to_jpg
-end
+generate_barcode("D\x08\x12", "f2")
+generate_barcode("D\x08\x13", "f3")
+generate_barcode("D\x08\x14", "f4")
+generate_barcode("D\x08\x16", "f6")
 
 
