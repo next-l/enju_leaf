b5c3cf528e9f6f3add4af073d7afeed195cde369
Emiko TAMIYA <emiko.tamiya@gmail.com>
2013-02-27 14:22:18 +0900

    refs 4712 no error without shelf on manifestation search results

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def call_numberformat(item = nil)
     call_number = item.call_number
     if SystemConfiguration.get("items.call_number.delete_first_delimiter")
-      delimiter = item.shelf.library.call_number_delimiter 
+      delimiter = item.try(:shelf).try(:library).try(:call_number_delimiter) || '|'
       if call_number
         if call_number.slice(0, 1) == delimiter
           call_number.slice!(0, 1)
