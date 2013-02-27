28697fb7f001d61dd90d4c495f8855697de92a70
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-02-28 00:36:58 +0900

    avoid FOUC

--- app/assets/javascripts/menu.enju.js
+++ app/assets/javascripts/menu.enju.js
@@ -1,7 +1,10 @@
+document.documentElement.className = 'hidden';
+
 $(function(){
   $('#bar1').menubar({
     position:{
       within: $("#demo-frame").add(window).first()
     }
   });
+  $('.hidden').removeClass('hidden');
 });
