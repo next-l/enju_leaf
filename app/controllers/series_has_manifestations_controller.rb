7926fb2c71d58deb1635f634e8ddfb2e55ade779
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-03-13 20:22:59 +0900

    refs4845 シリーズ情報と資料の関連の修正

--- app/controllers/series_has_manifestations_controller.rb
+++ app/controllers/series_has_manifestations_controller.rb
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 class SeriesHasManifestationsController < ApplicationController
   load_and_authorize_resource
-  before_filter :get_manifestation, :only => [:index, :new, :edit, :destroy]
-  before_filter :get_series_statement, :only => [:index, :new, :edit]
+  before_filter :get_manifestation#, :only => [:index, :new, :edit, :create, :destroy]
+  before_filter :get_series_statement, :only => [:index, :new, :create, :edit, :update]
   cache_sweeper :page_sweeper, :only => [:create, :update, :destroy]
 
   # GET /series_has_manifestations
@@ -49,8 +49,12 @@ class SeriesHasManifestationsController < ApplicationController
   def create
     respond_to do |format|
       if @series_has_manifestation.save
-        format.html { redirect_to @series_has_manifestation, :notice => t('controller.successfully_created', :model => t('activerecord.models.series_has_manifestation')) }
-        format.json { render :json => @series_has_manifestation, :status => :created, :location => @series_has_manifestation }
+        if params[:mode] == 'edit_manifestation'
+          format.html { redirect_to edit_manifestation_path(@series_has_manifestation.manifestation) }
+        else
+          format.html { redirect_to @series_has_manifestation, :notice => t('controller.successfully_created', :model => t('activerecord.models.series_has_manifestation')) }
+          format.json { render :json => @series_has_manifestation, :status => :created, :location => @series_has_manifestation }
+        end
       else
         format.html { render :action => "new" }
         format.json { render :json => @series_has_manifestation.errors, :status => :unprocessable_entity }
@@ -63,8 +67,12 @@ class SeriesHasManifestationsController < ApplicationController
   def update
     respond_to do |format|
       if @series_has_manifestation.update_attributes(params[:series_has_manifestation])
-        format.html { redirect_to @series_has_manifestation, :notice => t('controller.successfully_updated', :model => t('activerecord.models.series_has_manifestation')) }
-        format.json { head :no_content }
+        if params[:mode] == 'edit_manifestation'
+          format.html { redirect_to edit_manifestation_path(@series_has_manifestation.manifestation) }
+        else
+          format.html { redirect_to @series_has_manifestation, :notice => t('controller.successfully_updated', :model => t('activerecord.models.series_has_manifestation')) }
+          format.json { head :no_content }
+        end
       else
         format.html { render :action => "edit" }
         format.json { render :json => @series_has_manifestation.errors, :status => :unprocessable_entity }
@@ -79,10 +87,14 @@ class SeriesHasManifestationsController < ApplicationController
 
     respond_to do |format|
       format.html {
-        if @manifestation
-          redirect_to(manifestation_series_statements_url(@manifestation))
+        if params[:mode] == 'edit_manifestation'
+          redirect_to edit_manifestation_path(@series_has_manifestation.manifestation)
         else
-          redirect_to(series_has_manifestations_url)
+          if @manifestation
+            redirect_to(manifestation_series_statements_url(@manifestation))
+          else
+            redirect_to(series_has_manifestations_url)
+          end
         end
       }
       format.json { head :no_content }
