da87373f75a1d5f624bf2468fd28820dbfaf92bc
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-10 16:34:41 +0900

    updated generator

--- lib/generators/enju_leaf/setup/setup_generator.rb
+++ lib/generators/enju_leaf/setup/setup_generator.rb
@@ -4,17 +4,25 @@ class EnjuLeaf::SetupGenerator < Rails::Generators::Base
   desc "Create a setup file for Next-L Enju"
 
   def copy_setup_files
-    #directory("db/fixtures", "db/fixtures")
-    #copy_file("config/application.yml", "config/application.yml")
-    #copy_file("db/seeds.rb", "db/seeds.rb")
-    #generate("devise:install")
-    #generate("devise", "User")
+    directory("db/fixtures", "db/fixtures")
+    directory("solr", "solr")
+    copy_file("config/application.yml", "config/application.yml")
+    remove_file "db/seeds.rb"
+    copy_file("db/seeds.rb", "db/seeds.rb")
+    generate("devise:install")
+    generate("devise", "User")
+    rake("enju_leaf_engine:install:migrations")
+    rake("enju_biblio_engine:install:migrations")
+    rake("enju_library_engine:install:migrations")
     gsub_file 'config/routes.rb', /devise_for :users$/, "devise_for :users, :path => 'accounts'"
-    gsub_file 'config/initializers/devise.rb', '# config.email_regexp = /\A[^@]+@[^@]+\z/',
-      'config.email_regexp = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i'
-    #rake("enju_leaf_engine:install:migrations")
-    #rake("enju_biblio_engine:install:migrations")
-    #rake("enju_library_engine:install:migrations")
+    gsub_file 'config/initializers/devise.rb', '# config.email_regexp = /\A[^@]+@[^@]+\z/', 'config.email_regexp = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i'
+    gsub_file 'config/initializers/devise.rb', '# config.authentication_keys = [ :email ]', 'config.authentication_keys = [ :username ]'
+    gsub_file 'app/models/user.rb', /, :registerable,$/, ', #:registerable,'
+    gsub_file 'app/models/user.rb', /, :trackable, :validatable$/, <<EOS
+, :trackable, #:validatable, 
+    :lockable, :lock_strategy => :none, :unlock_strategy => :none
+  enju_leaf_user_model
+EOS
     inject_into_class "app/controllers/application_controller.rb", ApplicationController do
       <<"EOS"
   enju_leaf
@@ -37,5 +45,8 @@ EOS
     inject_into_file "app/assets/stylesheets/application.css", :after => / *= require_self$\n/ do
       " *= require enju_leaf\n"
     end
+    generate("sunspot_rails:install")
+    remove_file "public/index.html"
+    remove_file "app/views/layouts/application.html.erb"
   end
 end
