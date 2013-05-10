45450c905857434aeebe8fbc3e7737fea4066fe1
Emiko TAMIYA <emiko.tamiya@gmail.com>
2013-05-10 13:57:13 +0900

    refs 5182 not use authorize in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -210,11 +210,11 @@ class ApplicationController < ActionController::Base
 
   def get_user_if_nil
     @user = User.where(:username => params[:user_id]).first if params[:user_id]
-    if @user
-      authorize! :show, @user
-    else
-      raise ActiveRecord::RecordNotFound
-    end
+#    if @user
+#      authorize! :show, @user
+#    else
+#      raise ActiveRecord::RecordNotFound
+#    end
     return @user
   end
 
