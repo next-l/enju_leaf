43c19ab3ed0a15ad31582c3caa3b16aa464f8d96
Emiko TAMIYA <emiko.tamiya@gmail.com>
2013-05-13 19:12:37 +0900

    refs 5249 no audio with ie8

--- app/assets/javascripts/enju_audio_control.js
+++ app/assets/javascripts/enju_audio_control.js
@@ -1,6 +1,11 @@
 function playSound(audiofilename) {
-  audio = new Audio("");
-  audio.autoplay = true;
-  audio.src = audiofilename;
+  var appVersion = window.navigator.appVersion.toLowerCase();
+  if (appVersion.indexOf("msie 8.") != -1) {
+    return
+  } else {
+    audio = new Audio("");
+    audio.autoplay = true;
+    audio.src = audiofilename;
+  }
   //audio.play();
 }
