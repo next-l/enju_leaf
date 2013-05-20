e6985bb73697591a71f5cccbd918a998fd61a94d
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-05-20 20:28:40 +0900

    enju_trunk_statistics,enju_trunk_illがgemで呼ばれていない場合の対処をした。

--- lib/plugins.rb
+++ lib/plugins.rb
@@ -2,5 +2,14 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'plugins', 'ext'))
 
 require 'enju_trunk_circulation' rescue nil
-require 'enju_trunk_ill' rescue nil
-require 'enju_trunk_statistics' rescue nil
+begin
+  require 'enju_trunk_ill' 
+rescue LoadError
+  puts "info: enju_trunk_ill error"
+end
+begin
+  require 'enju_trunk_statistics' 
+rescue LoadError
+  puts "info: enju_trunk_statistics"
+end
+ 
