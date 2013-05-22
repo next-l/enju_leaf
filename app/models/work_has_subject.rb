ed2ae241f9bd1ad2d3124168a5b6deaa34d68025
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-05-22 13:42:17 +0900

    refs5307 件名の一覧からの件名追加のエラー修正

--- app/models/work_has_subject.rb
+++ app/models/work_has_subject.rb
@@ -1,4 +1,5 @@
 require EnjuSubject::Engine.root.join('app', 'models', 'work_has_subject')
 class WorkHasSubject < ActiveRecord::Base
+  attr_accessible :subject_id, :work_id, :work, :subject, :position
   has_paper_trail
 end
