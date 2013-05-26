73967b85651ffceb5d866d5933fa73200b3036df
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-26 10:54:22 +0900

    fixed mobylette_config

--- lib/generators/enju_leaf/setup/setup_generator.rb
+++ lib/generators/enju_leaf/setup/setup_generator.rb
@@ -50,6 +50,7 @@ EOS
 
   mobylette__config do |config|
     config[:skip_xhr_requests] = false
+    config[:skip_user_agents] = Setting.enju.skip_mobile_agents.map{|a| a.to_sym}
   end
 
 EOS
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -4,12 +4,8 @@ class ApplicationController < ActionController::Base
   enju_biblio
   enju_library
 
-  private
-  def mobylette_options
-    @mobylette_options ||= ApplicationController.send(:mobylette_options).merge(
-      {
-        :skip_xhr_requests => false
-      }
-    )
+  mobylette_config do |config|
+    config[:skip_xhr_requests] = false
+    config[:skip_user_agents] = Setting.enju.skip_mobile_agents.map{|a| a.to_sym}
   end
 end
