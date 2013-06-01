f2fd055e84b2b0a420405e2d890fba6bc912bf0c
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-06-01 20:32:43 +0900

    fixed generator

--- lib/generators/enju_leaf/setup/setup_generator.rb
+++ lib/generators/enju_leaf/setup/setup_generator.rb
@@ -48,7 +48,7 @@ EOS
   enju_biblio
   enju_library
 
-  mobylette__config do |config|
+  mobylette_config do |config|
     config[:skip_xhr_requests] = false
     config[:skip_user_agents] = Setting.enju.skip_mobile_agents.map{|a| a.to_sym}
   end
