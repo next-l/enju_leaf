79227dc31953753cb139b75bc67f8da4f90e07fa
Emiko TAMIYA <tamiya.emiko@miraitsystems.jp>
2013-07-05 15:03:02 +0900

    refs 5535 not clear manifestation cache after item delete

--- app/models/item_sweeper.rb
+++ app/models/item_sweeper.rb
@@ -3,12 +3,11 @@ class ItemSweeper < ActionController::Caching::Sweeper
   observe Item
 
   def after_save(record)
-    item = Item.find(record.id)
-    manifestation = item.manifestation
-    expire_editable_fragment(item)
+    expire_editable_fragment(record)
+    manifestation = Item.try(:find, record.id).try(:manifestation) rescue nil
     if manifestation
-      expire_editable_fragment(item.manifestation, ['detail', 'show_list', 'holding'])
-      expire_page(:controller => :manifestations, :action => :show, :id => item.manifestation.id, :page => 'show_list')
+      expire_editable_fragment(manifestation, ['detail', 'show_list', 'holding'])
+      expire_page(:controller => :manifestations, :action => :show, :id => manifestation.id, :page => 'show_list')
     end
     record.patrons.each do |patron|
       expire_editable_fragment(patron)
