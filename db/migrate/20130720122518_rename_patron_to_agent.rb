88112441d13ff479169ebb3e619a9963fef792a2
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-07-23 19:22:58 +0900

    fixed migration file

--- db/migrate/20130720122518_rename_patron_to_agent.rb
+++ db/migrate/20130720122518_rename_patron_to_agent.rb
@@ -1,6 +1,9 @@
 class RenamePatronToAgent < ActiveRecord::Migration
   def up
     rename_table :patrons, :agents
+    rename_table :patron_types, :agent_types
+    rename_table :patron_relationships, :agent_relationships
+    rename_table :patron_relationship_types, :agent_relationship_types
     rename_table :patron_import_files, :agent_import_files
     rename_table :patron_import_results, :agent_import_results
     #rename_table :patron_merge_lists, :agent_merge_lists
@@ -12,6 +15,7 @@ class RenamePatronToAgent < ActiveRecord::Migration
     rename_column :donates, :patron_id, :agent_id
     #rename_column :agent_merges, :patron_id, :agent_id
     rename_column :participates, :patron_id, :agent_id
+    rename_column :agents, :patron_type_id, :agent_type_id
     rename_column :agents, :patron_identifier, :agent_identifier
     rename_index :creates, "index_creates_on_patron_id", "index_creates_on_agent_id"
     rename_index :realizes, "index_realizes_on_patron_id", "index_realizes_on_agent_id"
@@ -22,10 +26,14 @@ class RenamePatronToAgent < ActiveRecord::Migration
 
   def down
     rename_table :agents, :patrons
+    rename_table :agent_types, :patron_types
+    rename_table :agent_relationships, :patron_relationships
+    rename_table :agent_relationship_types, :patron_relationship_types
     rename_table :agent_import_files, :patron_import_files
     rename_table :agent_import_results, :patron_import_results
     #rename_table :agent_merge_lists, :patron_merge_lists
     #rename_table :agent_merges, :patron_merges
+    rename_column :patrons, :agent_type_id, :patron_type_id
     rename_column :patrons, :agent_identifier, :patron_identifier
     rename_column :creates, :agent_id, :patron_id
     rename_column :realizes, :agent_id, :patron_id
