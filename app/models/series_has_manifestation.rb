ebb700baaf18eaf9ff23a8c6fb2e424b37bbe401
Emiko TAMIYA <tamiya.emiko@miraitsystems.jp>
2013-07-31 22:15:34 +0900

    refs 5593 do not validate Manifestation related to SeriesHasManifestation to create series_has_manifestation

--- app/models/series_has_manifestation.rb
+++ app/models/series_has_manifestation.rb
@@ -2,10 +2,9 @@ class SeriesHasManifestation < ActiveRecord::Base
   belongs_to :series_statement
   belongs_to :manifestation
 
-  validates_associated :series_statement, :manifestation
+#  validates_associated :series_statement, :manifestation
   validates_presence_of :series_statement_id, :manifestation_id
   validates_uniqueness_of :manifestation_id, :scope => :series_statement_id
-  validate :set_series_statement, :set_manifestation
   attr_accessible :series_statement_id, :manifestation_id, :series_statement, :manifestation
 
   acts_as_list :scope => :series_statement_id
@@ -22,29 +21,5 @@ class SeriesHasManifestation < ActiveRecord::Base
   def reindex
     series_statement.try(:index)
     manifestation.try(:index)
-  end
-
-  def set_series_statement
-    if series_statement_id
-      series_statement = SeriesStatement.find(:first, :conditions => ["id=?", self.series_statement_id])
-      if series_statement
-        self.series_statement = series_statement 
-      else
-        errors.add(:series_statement_id, I18n.t('activerecord.errors.messages.invalid'))
-        return false
-      end
-    end
-  end
-
-  def set_manifestation
-    if manifestation_id
-      manifestation = Manifestation.find(:first, :conditions => ["id=?", self.manifestation_id])
-      if manifestation
-        self.manifestation = manifestation
-      else
-        errors.add(:manifestation_id, I18n.t('activerecord.errors.messages.invalid'))
-        return false
-      end
-    end
   end
 end
