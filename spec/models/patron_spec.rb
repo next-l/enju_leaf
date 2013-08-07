85e4ec7f76bd34f7f01c3c4f77f51a96651fd461
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-07 21:00:47 +0900

    refs5613 Patronのテストの追加

--- spec/models/patron_spec.rb
+++ spec/models/patron_spec.rb
@@ -5,6 +5,202 @@ describe Patron do
   #pending "add some examples to (or delete) #{__FILE__}"
   fixtures :all
 
+  describe "バリデーションテスト: " do
+    before(:each) do
+      @patron = patrons(:patron_00003)
+    end
+
+    it "入力値が全て正しいとき、エラーは発生しないこと" do
+      @patron.should be_valid
+    end
+    # validates_presence_of :language, :patron_type, :country
+    # validates_associated :language, :patron_type, :country
+    it "language が不正のとき、エラーが発生すること" do
+      @patron.language = nil
+      expect(@patron).to have(2).errors_on(:language)
+    end 
+    it "patron_type が不正のとき、エラーが発生すること" do
+      @patron.patron_type = nil
+      expect(@patron).to have(2).errors_on(:patron_type)
+    end
+    it "country が不正のとき、エラーが発生すること" do
+      @patron.country = nil
+      expect(@patron).to have(2).errors_on(:country)
+    end
+    # validates :full_name, :presence => true, :length => {:maximum => 255}
+    it "full_name が255字より大きとき、エラーが発生すること" do
+      full_name = "a" * 256
+      @patron.full_name = full_name
+      @patron.should_not be_valid
+    end
+    #validates :user_id, :uniqueness => true, :allow_nil => true
+    it "user_id がユニークでないとき、エラーが発生すること" do
+      @patron.user = users(:admin) 
+      @patron.should_not be_valid
+    end
+    # validates :birth_date, :format => {:with => /^\d+(-\d{0,2}){0,2}$/}, :allow_blank => true
+    it "birth_date のフォーマットが不正なとき、エラーが発生すること" do
+      # case1 年、月、日のみ
+      # case1_1. YYYY       => OK
+      @patron.birth_date = '2000'
+      @patron.should be_valid 
+      # case1_2. MM        => OK
+      @patron.birth_date = '01'
+      @patron.should be_valid 
+      # case1_3. D        => OK
+      @patron.birth_date = '1'
+      @patron.should be_valid 
+      # case2 年月
+      # case2_1. YYYY-MM    => OK
+      @patron.birth_date = '2000-01'
+      @patron.should be_valid
+      # case2_2. YYYY-M     => OK
+      @patron.birth_date = '2000-1'
+      @patron.should be_valid
+      # case2_3. YYYYMM     => OK
+      @patron.birth_date = '200001'
+      @patron.should be_valid 
+      # case2_4. YYYYM      => OK
+      @patron.birth_date = '20001'
+      @patron.should be_valid 
+      # case3 年月日
+      # case3_1. YYYY-MM-DD => OK
+      @patron.birth_date = '2000-01-01'
+      @patron.should be_valid
+      # case3_2. YYYY-MM-D  => OK
+      @patron.birth_date = '2000-01-1'
+      @patron.should be_valid
+      # case3_3. YYYYMMDD   => OK
+      @patron.birth_date = '20000101'
+      @patron.should be_valid 
+      # case3_4. YYYYMMD    => OK
+      @patron.birth_date = '2000011'
+      @patron.should be_valid 
+      # case3_5. YYYY-MMDD  => NG
+      @patron.birth_date = '2000-0101'
+      @patron.should_not be_valid 
+      # case3_6. YYYY-MM-DDD=> NG
+      @patron.birth_date = '2000-01-011'
+      @patron.should_not be_valid 
+      # case4 他
+      # case4_1. STRING     => NG
+      @patron.birth_date = '平成元年'
+      @patron.should_not be_valid 
+      # case4_2. /          => NG
+      @patron.birth_date = '2000/01/01'
+      @patron.should_not be_valid 
+      # case4_3. .          => NG
+      @patron.birth_date = '2000.01.01'
+      @patron.should_not be_valid 
+    end
+    # validates :death_date, :format => {:with => /^\d+(-\d{0,2}){0,2}$/}, :allow_blank => true
+    it "death_date のフォーマットが不正なとき、エラーが発生すること" do
+      @patron.birth_date = '1900'
+      # case1 年、月、日のみ
+      # case1_1. YYYY       => OK
+      @patron.death_date = '2000'
+      @patron.should be_valid 
+      # case1_2. MM        => OK
+      @patron.death_date = '01'
+      @patron.should be_valid 
+      # case1_3. D        => OK
+      @patron.death_date = '1'
+      @patron.should be_valid 
+      # case2 年月
+      # case2_1. YYYY-MM    => OK
+      @patron.death_date = '2000-01'
+      @patron.should be_valid
+      # case2_2. YYYY-M     => OK
+      @patron.death_date = '2000-1'
+      @patron.should be_valid
+      # case2_3. YYYYMM     => OK
+      @patron.death_date = '200001'
+      @patron.should be_valid 
+      # case2_4. YYYYM      => OK
+      @patron.death_date = '20001'
+      @patron.should be_valid 
+      # case3 年月日
+      # case3_1. YYYY-MM-DD => OK
+      @patron.death_date = '2000-01-01'
+      @patron.should be_valid
+      # case3_2. YYYY-MM-D  => OK
+      @patron.death_date = '2000-01-1'
+      @patron.should be_valid
+      # case3_3. YYYYMMDD   => OK
+      @patron.death_date = '20000101'
+      @patron.should be_valid 
+      # case3_4. YYYYMMD    => OK
+      @patron.death_date = '2000011'
+      @patron.should be_valid 
+      # case3_5. YYYY-MMDD  => NG
+      @patron.death_date = '2000-0101'
+      @patron.should_not be_valid 
+      # case3_6. YYYY-MM-DDD=> NG
+      @patron.death_date = '2000-01-011'
+      @patron.should_not be_valid 
+      # case4 他
+      # case4_1. STRING     => NG
+      @patron.death_date = '平成元年'
+      @patron.should_not be_valid 
+      # case4_2. /          => NG
+      @patron.death_date = '2000/01/01'
+      @patron.should_not be_valid 
+      # case4_3. .          => NG
+      @patron.death_date = '2000.01.01'
+      @patron.should_not be_valid 
+    end
+    # validates :email, :format => {:with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i}, :allow_blank => true
+    it "email のフォーマットが不正なとき、エラーが発生すること" do
+      # case1. blank
+      @patron.email = ''
+      @patron.should be_valid
+      # case2. right
+      @patron.email = 'enju@example.com'
+      @patron.should be_valid
+      # case3. wrong
+      @patron.email = 'enjuexample.com'
+      @patron.should_not be_valid
+    end
+    # validate :check_birth_date
+    it "生年月日が没年月日より後のとき、エラーが発生すること" do
+      # case1. right
+      @patron.birth_date = '2000-01-01'
+      @patron.death_date = '2000-01-01'
+      @patron.should be_valid
+      # case2. wrong
+      @patron.birth_date = '2000-01-02'
+      @patron.death_date = '2000-01-01'
+      @patron.should_not be_valid
+    end 
+    # validate :check_duplicate_user
+    it "ユーザ情報の重複チェックを行うか" do
+      duplicate_patron     = Patron.new(:full_name => 'Kosuke Tanabe', :birth_date => '2000-01-01')
+      not_duplicate_patron = Patron.new(:full_name => 'Kosuke Tanabe', :birth_date => '2000-01-02')
+
+      # case1. duplicate
+      system_configuration = SystemConfiguration.find_by_keyname('patron.check_duplicate_user')
+      system_configuration.v = true
+      system_configuration.save 
+      duplicate_patron.should_not be_valid
+      not_duplicate_patron.should be_valid
+      # case2. it do not check duplication
+      system_configuration = SystemConfiguration.find_by_keyname('patron.check_duplicate_user')
+      system_configuration.v = false
+      system_configuration.save 
+      duplicate_patron.should be_valid
+      not_duplicate_patron.should be_valid
+    end
+  end 
+ 
+  # 以後、メソッドに関するテスト :TODO
+  it "should full_name without space" do
+    patron = Patron.new(:full_name => 'enju taro') 
+    patron.full_name_without_space.should eq 'enjutaro'
+  end
+
+  it "should set role and name" do
+  end
+
   it "should set a default required_role to Guest" do
     patron = FactoryGirl.create(:patron)
     patron.required_role.should eq Role.find_by_name('Guest')
