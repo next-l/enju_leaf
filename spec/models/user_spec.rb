3de11c17ee4c2b0021b045dbec7a994624a2e1a8
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-19 19:17:34 +0900

    refs5614 Userモデルテストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,156 @@ describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
   fixtures :all
 
+  describe 'validates' do
+    describe 'username' do
+      #validates :username, :presence => true, :format => {:with => /\A[0-9A-Za-z_]+\Z/, :message => I18n.t('errors.messages.en_expected')} 
+      context 'when username is correct' do
+        subject { FactoryGirl.build(:user, :username => 'test_00001') }
+        it { should be_valid }
+      end
+      context 'when has errores' do
+        before do
+          @user = FactoryGirl.build(:user, :username => nil)
+          @user.valid?
+        end
+        subject { @user.errors.messages[:username] }
+        it('should return error message') { should include(I18n.t('errors.messages.en_expected')) }
+      end
+      context 'when username is nil' do
+        subject { FactoryGirl.build(:user, :username => nil) }
+        it { should_not be_valid } 
+      end
+      context 'when username is wrong format' do
+        shared_examples_for 'create user with wrong format' do
+          subject { FactoryGirl.build(:user, :username => username) }
+          it { should_not be_valid }
+        end 
+        it_behaves_like 'create user with wrong format' do let(:username) { 'テスト' }     end
+        it_behaves_like 'create user with wrong format' do let(:username) { 'test 00001' } end
+        it_behaves_like 'create user with wrong format' do let(:username) { 'test-00001' } end
+        it_behaves_like 'create user with wrong format' do let(:username) { 'test/00001' } end
+        it_behaves_like 'create user with wrong format' do let(:username) { 'test.00001' } end
+      end
+      #validates_uniqueness_of :username, :unless => proc{|user| SystemConfiguration.get('auto_user_number')}
+      context 'when duplication username' do
+        shared_examples_for 'system_confiduration set value on auto_user_number' do
+          subject { FactoryGirl.build(:user, :username => 'admin') }
+          it "check unique" do
+            system_configuration = SystemConfiguration.find_by_keyname('auto_user_number')
+            system_configuration.v = value
+            system_configuration.save
+            Rails.cache.clear
+            result
+          end
+        end
+        it_behaves_like 'system_confiduration set value on auto_user_number' do
+          let(:value)  { 'true' }
+          let(:result) { should_not be_valid }
+        end 
+        it_behaves_like 'system_confiduration set value on auto_user_number' do
+          let(:value)  { 'false' }
+          let(:result) { should be_valid }
+        end 
+      end
+    end
+    describe 'email' do
+      context 'email is nil' do
+        subject { FactoryGirl.build(:user, :email => nil) }
+        it { should be_valid }
+      end
+      #validates_uniqueness_of :email, :scope => authentication_keys[1..-1], :case_sensitive => false, :allow_blank => true
+      describe 'uniquness' do
+        context 'when not duplication email' do
+          subject { FactoryGirl.build(:user, :email => "#{Time.now.strftime("%m%d%I%M").to_s}@exsample.com") }
+          it { should be_valid } 
+        end
+        context 'when duplication email' do
+          subject { FactoryGirl.build(:user, :email => users(:admin).email) }
+          it { should_not be_valid } 
+        end
+        context 'when duplication email is uppercase' do
+          subject { FactoryGirl.build(:user, :email => users(:admin).email.upcase) }
+          it { should_not be_valid } 
+        end
+      end
+      #validates :email, :format => {:with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i}, :allow_blank => true
+      describe 'format' do
+        context 'format of email is correct' do
+          shared_examples_for 'create user has email of correct format' do
+            subject { FactoryGirl.build(:user, :email => email) }
+            it { should be_valid }
+          end
+          it_behaves_like 'create user has email of correct format' do let(:email) { '' }                 end
+          it_behaves_like 'create user has email of correct format' do let(:email) { ' ' }                end
+          it_behaves_like 'create user has email of correct format' do let(:email) { 'a@example.com' }    end
+          it_behaves_like 'create user has email of correct format' do let(:email) { '.@example.com' }    end
+          it_behaves_like 'create user has email of correct format' do let(:email) { '%@example.com' }    end
+          it_behaves_like 'create user has email of correct format' do let(:email) { '+@example.com' }    end
+          it_behaves_like 'create user has email of correct format' do let(:email) { '-@example.com' }    end
+          it_behaves_like 'create user has email of correct format' do let(:email) { 'enju@example.jp' }  end
+          it_behaves_like 'create user has email of correct format' do let(:email) { 'enju@example.com' } end
+        end
+        context 'format of email is wrong' do
+          shared_examples_for 'create user has email of wrong format' do
+            subject { FactoryGirl.build(:user, :email => email) }
+            it { should_not be_valid }
+          end
+          chracters = %w-! # $ & ' * / = ? ^  ` { | } ~ ( ) < > [ ] : ; @ ,-
+          chracters2 = %w-! # $ & ' * / = ? ^  ` { | } ~ ( ) < > [ ] : ; @ , . % +-
+          it_behaves_like 'create user has email of wrong format' do let(:email) { '@' }              end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { '@.' }             end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { 'enju@' }          end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { 'enju@' }          end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { 'enju@example' }   end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { 'enju@example.c' } end
+          it_behaves_like 'create user has email of wrong format' do let(:email) { 'enjuexample.com' } end
+          chracters.map  { |char| it_behaves_like 'create user has email of wrong format' do let(:email) { "#{char}@example.com" }      end }
+          chracters2.map { |char| it_behaves_like 'create user has email of wrong format' do let(:email) { "enju@#{char}.com" }         end }
+          chracters2.map { |char| it_behaves_like 'create user has email of wrong format' do let(:email) { "enju@example.#{char * 2}" } end }
+        end
+      end
+=begin
+TODO: 
+      describe 'confirmation of email' do
+        #validates_confirmation_of :email, :on => :create#, :if => proc{|user| !user.operator.try(:has_role?, 'Librarian')}
+        context 'when create user' do
+          context '' do 
+            subject { FactoryGirl.build(:user, { email: 'enju@exsample.com', email_confirmation: 'enju@exsample.com' }) }
+            it { should be_valid }
+          end
+        end
+        #validates_confirmation_of :email, :on => :update, :if => Proc.new { |user| user.email != User.find(user.id).email }
+        context 'when update user' do
+        end
+      end
+=end
+    end
+    describe 'expired_at' do
+      #validates_date :expired_at, :allow_blank => true
+    end
+  end
+=begin
+  with_options :if => :password_required? do |v|
+    v.validates_presence_of     :password
+    v.validates_confirmation_of :password
+    v.validates_length_of       :password, :within => 6..20, :allow_blank => true
+  end
+
+  validates_associated :patron, :user_group, :library
+  validates_presence_of :user_group, :library, :locale
+  validates :user_number, :uniqueness => true, :format => {:with => /\A[0-9A-Za-z_]+\Z/}, :allow_blank => true
+  before_validation :set_role_and_patron, :on => :create
+  before_validation :set_lock_information
+  before_validation :set_user_number, :on => :create
+  before_destroy :check_item_before_destroy, :check_role_before_destroy
+  before_save :check_expiration
+  before_create :set_expired_at
+  after_destroy :remove_from_index
+  after_create :set_confirmation
+  after_save :index_patron
+  after_destroy :index_patron
+=end
+=begin
   it 'should create an user' do
     FactoryGirl.create(:user)
   end
@@ -215,6 +365,7 @@ describe User do
     sort4[:sort_by].should eq 'created_at'
     sort4[:order].should eq 'desc'
   end
+=end
 end
 
 # == Schema Information
