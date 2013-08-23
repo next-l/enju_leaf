f1ac9536c4be2b7ff56bff743a2f7a6255c99f77
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-23 16:48:04 +0900

    refs5672 move spec file of checked_item from enju_trunk to enju_trunk_circulation

--- spec/models/checked_item_spec.rb
+++ spec/models/checked_item_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 
 describe CheckedItem do
   fixtures :all
-
-  it "should respond to available_for_checkout?" do
-    checked_items(:checked_item_00001).available_for_checkout?.should_not be_true
-  end
 end
 
 # == Schema Information
