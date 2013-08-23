248624c8a205f41db8446ea5dc994f6e2b5f93ba
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-08-23 17:09:10 +0900

    refs 5675 jQuery関係のファイルの整理 gem側のファイルを利用するようにした。 重複しているファイルを削除した。 event_calendarのjsがjQueryのバージョンアップにより動作しなくなったので修正した。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,14 @@
 //= require jquery
-//= require jquery-ui
-//= require jquery.colorbox-min
 //= require jquery_ujs
+//= require jquery.ui.all
+//= require jquery.colorbox-min
 //= require jquery.highlight-3
 //= require jquery.hotkeys
 //= require jquery.cookie
 //= require jquery.keypad
+//= require select2
 //= require fg.menu
 //= require fg.menu.enju
-//= require pagination
 //= require select_locale
 //= require portlets
 //= require tab_view
@@ -17,7 +17,6 @@
 //= require function_key_control
 //= require enju_audio_control
 //= require enju_trunk
-//= require jquery.ui.datepicker-ja
 //= require jquery.spin
 //= require jquery.upload-1.0.2
 //= require jquery.simplecalendarjp
