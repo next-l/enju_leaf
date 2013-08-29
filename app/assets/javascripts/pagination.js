bbf5471efefcbf45b061bac1ac40248c6a6018cf
Emiko TAMIYA <tamiya.emiko@miraitsystems.jp>
2013-08-29 19:24:39 +0900

    update jquery 1.10

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,9 +1,9 @@
 $(function() {
-  $(".content_pagination a").live("click", function() {
+  $(".content_pagination a").on("click", function() {
     $.get(this.href, null, null, "script");
     return false;
   });
-  $(".sidebar_pagination a").live("click", function() {
+  $(".sidebar_pagination a").on("click", function() {
     $.get(this.href, null, null, "script");
     return false;
   });
