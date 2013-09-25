820aa6132a207041b9a38fb1afcc0a464c0df9cc
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-09-25 10:47:12 +0900

    refs 5744 バリデーションの追加指定

--- app/models/thema.rb
+++ app/models/thema.rb
@@ -1,3 +1,9 @@
 class Thema < ActiveRecord::Base
   attr_accessible :description, :desplay_name, :name, :note, :position, :publish
+
+  # TODO: 以下のバリデーションを追加してください
+  # name はユニークであること
+  # name, display_name, position, publish が空白登録できないこと
+  # TODO: manifestation_idと関連を作成後の作業
+  # manifestation との関連の検証
 end
