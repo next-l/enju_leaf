caf8c80caa71f1088435c31044805a0567b6cf4a
morimoto002 <morimoto.tatsuya002@miraitsystem.jp>
2013-09-27 13:03:36 +0900

    refs 5744  誤って上げたConfig内のファイル削除

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-EnjuLeaf::Application.config.secret_token = '71a676c67a3046e2d81e26b0d6e495109f9cea93e44a022c61804097e72bc8a114a5b6ada5c27d87dd52994963d2e16f30f2f017da0ad1720d7edc5669de0278'
