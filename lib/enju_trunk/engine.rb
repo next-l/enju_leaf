d0895937128ff5bbf653ec1687179222d06cce03
Kosuke Tanabe <kosuke@e23.jp>
2013-12-16 01:17:58 +0900

    ライブラリの読み込みを修正

--- lib/enju_trunk/engine.rb
+++ lib/enju_trunk/engine.rb
@@ -2,6 +2,12 @@ require 'rghost'
 require 'inherited_resources'
 require 'RedCloth'
 require 'spinjs-rails'
+require "geocoder"
+require "client_side_validations"
+require "acts-as-taggable-on"
+require "paper_trail"
+require "simple_form"
+require "select2-rails"
 
 module EnjuTrunk
   class Engine < ::Rails::Engine
