b89687b280fe1adae052dcbb283d4adab763367b
Kosuke Tanabe <nabeta@fastmail.fm>
2014-05-31 23:23:09 +0900

    updated template files

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     mount Resque::Server.new, :at => "/resque", :as => :resque
   end
 
-  resources :user_import_results
+  resources :user_import_results, :only => [:index, :show, :destroy]
 
   resources :user_import_files do
-    resources :user_import_results
+    resources :user_import_results, :only => [:index, :show, :destroy]
   end
 
   resource :my_account
