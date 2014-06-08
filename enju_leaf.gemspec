21761c6adb761b25c059ab121637c14476a0e254
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-08 23:45:00 +0900

    updated gemspec
    Conflicts:
    	enju_leaf.gemspec

--- enju_leaf.gemspec
+++ enju_leaf.gemspec
@@ -29,7 +29,6 @@ Gem::Specification.new do |s|
   s.add_dependency "rails_autolink"
   s.add_dependency "jquery-ui-rails"
   s.add_dependency "resque-scheduler", "~> 3.0"
-  #s.add_dependency "protected_attributes"
   s.add_dependency "paperclip", "~> 4.1"
   if RUBY_PLATFORM == "java"
     s.add_dependency "kramdown"
---
+++ spec/routing/resource_import_files_routing_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe UserImportFilesController do
+  describe "routing" do
+
+    it "routes to #index" do
+      get("/user_import_files").should route_to("user_import_files#index")
+    end
+
+    it "routes to #new" do
+      get("/user_import_files/new").should route_to("user_import_files#new")
+    end
+
+    it "routes to #show" do
+      get("/user_import_files/1").should route_to("user_import_files#show", :id => "1")
+    end
+
+    it "routes to #edit" do
+      get("/user_import_files/1/edit").should route_to("user_import_files#edit", :id => "1")
+    end
+
+    it "routes to #create" do
+      post("/user_import_files").should route_to("user_import_files#create")
+    end
+
+    it "routes to #update" do
+      put("/user_import_files/1").should route_to("user_import_files#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      delete("/user_import_files/1").should route_to("user_import_files#destroy", :id => "1")
+    end
+
+  end
+end
