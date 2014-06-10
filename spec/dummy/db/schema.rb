837d7cb36d663ac912f274e8ce2619305100ed17
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-10 21:51:42 +0900

    email may be blank next-l/enju_leaf#144

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140110131010) do
+ActiveRecord::Schema.define(:version => 20140610123439) do
 
   create_table "accepts", :force => true do |t|
     t.integer  "basket_id"
@@ -546,6 +546,24 @@ ActiveRecord::Schema.define(:version => 20140110131010) do
   add_index "produces", ["agent_id"], :name => "index_produces_on_agent_id"
   add_index "produces", ["manifestation_id"], :name => "index_produces_on_manifestation_id"
 
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "user_group_id"
+    t.integer  "library_id"
+    t.string   "locale"
+    t.string   "user_number"
+    t.text     "full_name"
+    t.text     "note"
+    t.text     "keyword_list"
+    t.integer  "required_role_id"
+    t.string   "state"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id"
+  add_index "profiles", ["user_number"], :name => "index_profiles_on_user_number"
+
   create_table "realize_types", :force => true do |t|
     t.string   "name"
     t.text     "display_name"
@@ -821,7 +839,7 @@ ActiveRecord::Schema.define(:version => 20140110131010) do
     t.datetime "confirmed_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
   add_index "users", ["user_group_id"], :name => "index_users_on_user_group_id"
