b715b95d55fc8dfbc91a3b44ef2f6b5567be9371
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-25 23:17:03 +0900

    updated index definition

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   belongs_to :user
   belongs_to :library, :validate => true
   belongs_to :user_group
@@ -8,8 +10,25 @@ class Profile < ActiveRecord::Base
   validates_presence_of :user_group, :library, :locale #, :user_number
   validates :user_number, :uniqueness => true, :format => {:with => /\A[0-9A-Za-z_]+\Z/}, :allow_blank => true
 
-  searchable do
-    text :user_number, :full_name, :note
-    string :user_number
+  index_name "#{name.downcase.pluralize}-#{Rails.env}"
+
+  after_commit on: :create do
+    index_document
+  end
+
+  after_commit on: :update do
+    update_document
+  end
+
+  after_commit on: :destroy do
+    delete_document
+  end
+
+  settings do
+    mappings dynamic: 'false', _routing: {required: true, path: :required_role_id} do
+      indexes :user_number
+      indexes :full_name
+      indexes :note
+    end
   end
 end
