7df6b259065de2a991787eb041fec3aa0baa70e7
Kosuke Tanabe <nabeta@fastmail.fm>
2014-07-10 15:22:01 +0900

    fixed exporting users

--- lib/enju_leaf/user.rb
+++ lib/enju_leaf/user.rb
@@ -134,7 +134,6 @@ module EnjuLeaf
               lines << nil
             end
             lines << u.note
-            note
           }
           if options[:format] == :tsv
             users.map{|u| u.join("\t")}.unshift(header).join("\r\n")
