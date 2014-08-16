8674b307c556689b10e5c32f2b8c6b61cc73eb95
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-16 19:21:08 +0900

    updated validation messages next-l/enju_leaf#325

--- lib/enju_leaf/master_model.rb
+++ lib/enju_leaf/master_model.rb
@@ -4,9 +4,11 @@ module MasterModel
     base.send :include, InstanceMethods
     base.class_eval do
       acts_as_list
-      validates_uniqueness_of :name, :case_sensitive => false,
-        :message => I18n.t('Only lowercase letters and numbers are allowed.')
-      validates :name, :presence => true, :format => {:with => /\A[0-9A-Za-z][0-9A-Za-z_\-\s]*[0-9a-z]\Z/}
+      validates_uniqueness_of :name, :case_sensitive => false
+      validates :name, :presence => true, :format => {
+        :with => /\A[0-9A-Za-z][0-9A-Za-z_\-\s]*[0-9a-z]\Z/,
+        :message => I18n.t('page.only_lowercase_letters_and_numbers_are_allowed')
+      }
       validates :display_name, :presence => true
       before_validation :set_display_name, :on => :create
       normalize_attributes :name
