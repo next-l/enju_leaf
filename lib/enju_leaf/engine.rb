4f086c650b5495185466e792fe053fdffa14e81d
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-02 04:00:29 +0900

    reverted pagination css

--- lib/enju_leaf/engine.rb
+++ lib/enju_leaf/engine.rb
@@ -11,7 +11,6 @@ require 'jquery-rails'
 require 'jquery-ui-rails'
 require 'statesman'
 require 'resque/scheduler/server'
-require 'protected_attributes' if Rails::VERSION::MAJOR == 4
 
 module EnjuLeaf
   class Engine < ::Rails::Engine
