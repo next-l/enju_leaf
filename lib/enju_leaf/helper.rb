93d5706570981a10f9a86fc0e5e02951c9aaf2ef
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-08 00:26:57 +0900

    added messages_count

--- lib/enju_leaf/helper.rb
+++ lib/enju_leaf/helper.rb
@@ -112,5 +112,12 @@ module EnjuLeaf
         markdown.render(string.to_s).html_safe
       end
     end
+
+    def messages_count(user)
+      Message.search do
+        with(:receiver_id).equal_to user.id
+        with(:is_read).equal_to false
+      end.hits.total_entries
+    end
   end
 end
