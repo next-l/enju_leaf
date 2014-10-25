e23e80c25fd228a2c72540e5c4fe533d3b698440
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-26 04:47:41 +0900

    require codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
-require 'coveralls'
+require "codeclimate-test-reporter"
 SimpleCov.start 'rails'
-Coveralls.wear!
+CodeClimate::TestReporter.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
