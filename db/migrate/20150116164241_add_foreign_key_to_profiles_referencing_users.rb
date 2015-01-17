fb1d8ab690213029b70414210f5525c0204ec164
Kosuke Tanabe <nabeta@fastmail.fm>
2015-01-18 03:55:16 +0900

    updated migration file

--- db/migrate/20150116164241_add_foreign_key_to_profiles_referencing_users.rb
+++ db/migrate/20150116164241_add_foreign_key_to_profiles_referencing_users.rb
@@ -1,5 +1,7 @@
 class AddForeignKeyToProfilesReferencingUsers < ActiveRecord::Migration
   def change
     add_foreign_key :profiles, :users, on_delete: :nullify
+    add_foreign_key :profiles, :library
+    add_foreign_key :profiles, :user_group
   end
 end
