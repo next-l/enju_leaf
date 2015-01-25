7f0644d548697edf898eb89942e147cd250ac232
Kosuke Tanabe <nabeta@fastmail.fm>
2015-01-25 14:07:24 +0900

    added foreign_key to user_has_roles

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116164241) do
+ActiveRecord::Schema.define(version: 20150125050613) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
