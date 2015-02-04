099ac0ef5df28fc287bec61c3892f2035c874c58
Kosuke Tanabe <nabeta@fastmail.fm>
2015-02-04 19:44:18 +0900

    removed foreign key

--- db/migrate/20150116164241_add_foreign_key_to_profiles_referencing_users.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeyToProfilesReferencingUsers < ActiveRecord::Migration
-  def change
-    add_foreign_key :profiles, :users
-    add_foreign_key :profiles, :libraries
-    add_foreign_key :profiles, :user_groups
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116164241) do
+ActiveRecord::Schema.define(version: 20141014065831) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
