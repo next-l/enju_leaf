0f919f4d4e5f588d98102c4a10892079cce956d1
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-25 14:19:11 +0900

    fixed migration file

--- db/migrate/20150304175658_add_user_export_id_to_user_export_file.rb
+++ db/migrate/20150304175658_add_user_export_id_to_user_export_file.rb
@@ -1,9 +1,8 @@
 class AddUserExportIdToUserExportFile < ActiveRecord::Migration
   def change
     add_column :user_export_files, :user_export_id, :string
-    add_column :user_export_files, :user_export_file_name, :string
     add_column :user_export_files, :user_export_size, :integer
-    add_column :user_export_files, :user_export_content_type, :string
+    rename_column :user_export_files, :user_import_file_name, :user_export_filename
     add_index :user_export_files, :user_export_id
   end
 end
