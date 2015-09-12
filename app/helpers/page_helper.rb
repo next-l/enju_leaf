8ff3bc580e57c62dc338a68fe6cfd7707b26acf6
Masao Takaku <tmasao@acm.org>
2015-09-12 16:23:18 +0900

    add buttons for Check all and Uncheck all. cf. #603

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,2 +1,21 @@
 module PageHelper
+  def check_all_button( param, label, checked = true )
+    func_name = "check_all"
+    func_name = "un" + func_name if checked == false
+    html = <<-EOF
+    <button id="#{ func_name }_#{ param }">#{ label }</button>
+    <script>
+      $(function(){
+        $("##{ func_name }_#{ param }").click(function(){
+          $("input##{ param }").prop("checked", #{ checked });
+	  return false;
+        });
+      });
+    </script>
+    EOF
+    html.html_safe
+  end
+  def uncheck_all_button( param, label )
+    check_all_button( param, label, false )
+  end
 end
