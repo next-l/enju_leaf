d2ff9283ac389064674ab45889e72f141ff26156
Kosuke Tanabe <nabeta@fastmail.fm>
2015-11-24 00:34:38 +0900

    move CalculateStat and MasterModel modules

--- lib/enju_leaf.rb
+++ lib/enju_leaf.rb
@@ -2,9 +2,7 @@ require "enju_leaf/engine"
 require "enju_leaf/version"
 require "enju_leaf/controller"
 require "enju_leaf/helper"
-require "enju_leaf/calculate_stat"
 require "enju_leaf/localized_name"
-require "enju_leaf/master_model"
 require "enju_leaf/url_validator"
 
 require 'csv'
