465e849885adb077e155df953f3017e3f725337a
Kosuke Tanabe <nabeta@fastmail.fm>
2015-12-05 17:25:07 +0900

    update spec file

--- spec/views/profiles/edit.html.erb_spec.rb
+++ spec/views/profiles/edit.html.erb_spec.rb
@@ -9,7 +9,7 @@ describe "profiles/edit" do
     assign(:libraries, Library.all)
     assign(:roles, Role.all)
     assign(:available_languages, Language.available_languages)
-    view.stub(:current_user).and_return(User.find('enjuadmin'))
+    view.stub(:current_user).and_return(User.friendly.find('enjuadmin'))
   end
 
   describe "when logged in as librarian" do
@@ -17,10 +17,10 @@ describe "profiles/edit" do
       @profile = assign(:profile, profiles(:librarian2))
       user = users(:librarian1)
       view.stub(:current_user).and_return(user)
-      allow(view).to receive(:policy).and_return double(create?: true, update?: true, destroy?: true)
     end
 
     it "renders the edit user form" do
+      allow(view).to receive(:policy).and_return double(destroy?: true)
       render
       assert_select "form", action: profiles_path(@profile), method: "post" do
         assert_select "input#profile_user_number", name: "profile[user_number]"
@@ -28,11 +28,13 @@ describe "profiles/edit" do
     end
 
     it "should not display 'delete' link" do
+      allow(view).to receive(:policy).and_return double(destroy?: false)
       render
       expect(rendered).not_to have_selector("a[href='#{profile_path(@profile.id)}'][data-method='delete']") 
     end
 
     it "should disable role selection" do
+      allow(view).to receive(:policy).and_return double(destroy?: true)
       render
       expect(rendered).to have_selector("select#profile_user_attributes_user_has_role_attributes_role_id[disabled='disabled']")
     end
