60533cb6418cdd21d4d55d186b75c3ab85fc36a6
Kosuke Tanabe <nabeta@fastmail.fm>
2015-12-09 21:14:20 +0900

    move EnjuLeaf::Controller

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  enju_leaf
+  include EnjuLeaf::Controller
   enju_biblio
   enju_library
   after_action :verify_authorized
