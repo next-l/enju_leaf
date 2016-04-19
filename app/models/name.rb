aab664e8b40bf434343558cc199f0064b96f5d05
Kosuke Tanabe <nabeta@fastmail.fm>
2016-04-19 22:16:18 +0900

    add source to Name

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,18 +1,7 @@
 class Name < ActiveRecord::Base
+  belongs_to :manifestation
   belongs_to :profile
-end
+  validates :source, presence: true
 
-# == Schema Information
-#
-# Table name: names
-#
-#  id          :integer          not null, primary key
-#  first_name  :string
-#  middle_name :string
-#  last_name   :string
-#  language_id :integer
-#  profile_id  :integer
-#  position    :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
+  acts_as_list scope: :profile_id
+end
--- db/migrate/20151129070319_create_names.rb
+++ db/migrate/20151129070319_create_names.rb
@@ -4,12 +4,15 @@ class CreateNames < ActiveRecord::Migration
       t.string :first_name
       t.string :middle_name
       t.string :last_name
+      t.string :full_name
       t.integer :language_id
       t.integer :profile_id
       t.integer :position
+      t.string :source
 
       t.timestamps null: false
     end
     add_index :names, :profile_id
+    add_index :names, :full_name
   end
 end
