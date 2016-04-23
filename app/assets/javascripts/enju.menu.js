d5e2c12928f7c0b4f853253044e5e2c6809d6369
Kosuke Tanabe <nabeta@fastmail.fm>
2016-04-23 16:28:24 +0900

    update menu script

--- app/assets/javascripts/enju.menu.js
+++ app/assets/javascripts/enju.menu.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
-  $("#tabs").tabs();
+document.documentElement.className = 'hidden';
+
+$(function(){
   $('#bar1').menubar({
     position:{
       within: $("#demo-frame").add(window).first()
     }
   });
+  $('.hidden').removeClass('hidden');
+});
+
+$(document).ready(function(){
+  $("#tabs").tabs();
   $('.hidden').removeClass('hidden');
 });
