f33ba9e21aa16fde65a42d39b306d27563414c3c
Kosuke Tanabe <nabeta@fastmail.fm>
2016-07-07 02:10:04 +0900

    add set_paper_trail_whodunnit

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include EnjuLeaf::Controller
   include EnjuBiblio::Controller
   include EnjuLibrary::Controller
+  before_action :set_paper_trail_whodunnit
   after_action :verify_authorized
 
   include Pundit
