f1713286fb40af328ba4fd143ec34ad73ba7970c
Kosuke Tanabe <nabeta@fastmail.fm>
2018-06-10 16:29:24 +0900

    replace find_by_name

--- spec/factories/agent.rb
+++ spec/factories/agent.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :agent do |f|
     f.sequence(:full_name){|n| "full_name_#{n}"}
-    f.agent_type_id{AgentType.find_by_name('Person').id}
+    f.agent_type_id{AgentType.find_by(name: 'Person').id}
     f.country_id{Country.first.id}
     f.language_id{Language.first.id}
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     f.password_confirmation 'adminpassword'
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by_name('Administrator').id})
+      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'Administrator').id})
       user_has_role.save
       user.reload
     end
@@ -19,7 +19,7 @@ FactoryBot.define do
     f.password_confirmation 'librarianpassword'
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by_name('Librarian').id})
+      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'Librarian').id})
       user_has_role.save
       user.reload
     end
@@ -32,7 +32,7 @@ FactoryBot.define do
     f.password_confirmation 'userpassword'
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by_name('User').id})
+      user_has_role.assign_attributes({user_id: user.id, role_id: Role.find_by(name: 'User').id})
       user_has_role.save
       user.reload
     end
