c3ec73d208fa121edefa14a6e12f8a843b06da2e
Kosuke Tanabe <nabeta@fastmail.fm>
2018-09-23 14:43:40 +0900

    fix dummy file

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, # , :validatable
          :lockable, lock_strategy: :none, unlock_strategy: :none
