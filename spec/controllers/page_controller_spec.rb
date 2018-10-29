6e21cc028672df34685160141af0f875785176c6
Kosuke Tanabe <nabeta@fastmail.fm>
2018-10-29 19:42:31 +0900

    update spec file

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -9,17 +9,17 @@ describe PageController do
 
       it "should get import" do
         get :import
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get configuration" do
         get :configuration
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get system information" do
         get :system_information
-        expect(response).to be_success
+        expect(response).to be_successful
       end
     end
 
@@ -28,7 +28,7 @@ describe PageController do
 
       it "should redirect to user" do
         get :index
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should not get import" do
@@ -50,17 +50,17 @@ describe PageController do
     describe "When not logged in" do
       it "should get index" do
         get :index
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get opensearch" do
         get :opensearch, format: :xml
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get msie_accelerator" do
         get :msie_accelerator, format: :xml
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get routing_error" do
@@ -71,23 +71,23 @@ describe PageController do
 
       it "should get advanced_search" do
         get :advanced_search
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:libraries)).to eq Library.order(:position)
       end
 
       it "should get about" do
         get :about
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get add_on" do
         get :add_on
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should get statistics" do
         get :statistics
-        expect(response).to be_success
+        expect(response).to be_successful
       end
 
       it "should not get import" do
