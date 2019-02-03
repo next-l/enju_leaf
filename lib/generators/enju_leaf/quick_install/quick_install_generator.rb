ca6de1ae9e12fb480856f60c26d577fcb3d881b0
Kosuke Tanabe <nabeta@fastmail.fm>
2019-02-03 14:27:10 +0900

    add quick_setup generator

--- lib/generators/enju_leaf/quick_install/quick_install_generator.rb
+++ lib/generators/enju_leaf/quick_install/quick_install_generator.rb
@@ -3,24 +3,6 @@ class EnjuLeaf::QuickInstallGenerator < Rails::Generators::Base
 
   def quick_install
     environment = ENV['RAILS_ENV'] || 'development'
-    gsub_file 'config/schedule.rb', /^set :environment, :development$/,
-      "set :environment, :#{environment}"
-    rake("enju_seed_engine:install:migrations")
-    rake("enju_library_engine:install:migrations")
-    rake("enju_biblio_engine:install:migrations")
-    rake("enju_manifestation_viewer_engine:install:migrations")
-    if !ENV['ENJU_SKIP_CONFIG']
-      generate("enju_library:setup")
-      generate("enju_biblio:setup")
-      generate("enju_circulation:setup")
-      generate("enju_subject:setup")
-    end
-    rake("enju_ndl_engine:install:migrations")
-    append_to_file "app/models/user.rb", <<EOS.strip_heredoc
-      Manifestation.include(EnjuManifestationViewer::EnjuManifestation)
-      Manifestation.include(EnjuNdl::EnjuManifestation)
-EOS
-
     rake("db:migrate", env: environment)
     rake("enju_leaf:setup", env: environment)
     rake("enju_circulation:setup", env: environment)
