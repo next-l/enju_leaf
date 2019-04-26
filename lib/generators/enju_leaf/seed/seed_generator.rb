14592732b027fef4c4ccbf440f980cfa85228a57
Kosuke Tanabe <nabeta@fastmail.fm>
2019-04-26 12:10:07 +0900

    update seed generator

--- lib/generators/enju_leaf/seed/seed_generator.rb
+++ lib/generators/enju_leaf/seed/seed_generator.rb
@@ -3,21 +3,25 @@ class EnjuLeaf::SeedGenerator < Rails::Generators::Base
 
   def seed
     environment = ENV['RAILS_ENV'] || 'development'
-    rake("db:migrate", env: environment)
-    rake("enju_leaf:setup", env: environment)
-    rake("enju_circulation:setup", env: environment)
-    rake("enju_subject:setup", env: environment)
-    rake("assets:precompile", env: environment) if environment == 'production'
-    rake("db:seed", env: environment)
     if !ENV['ENJU_SKIP_SOLR']
       if ENV['OS'] == 'Windows_NT'
         rake("sunspot:solr:run", env: environment)
       else
         rake("sunspot:solr:start", env: environment)
         sleep 5
-        rake("environment sunspot:reindex", env: environment)
-        rake("sunspot:solr:stop", env: environment)
       end
+    end
+
+    rake("db:migrate", env: environment)
+    rake("enju_leaf:setup", env: environment)
+    rake("enju_circulation:setup", env: environment)
+    rake("enju_subject:setup", env: environment)
+    rake("assets:precompile", env: environment) if environment == 'production'
+    rake("db:seed", env: environment)
+
+    if !ENV['ENJU_SKIP_SOLR']
+      rake("environment sunspot:reindex", env: environment)
+      rake("sunspot:solr:stop", env: environment)
     end
 
     rake("enju_leaf:load_asset_files")
