22fc78fcd21f547d8eb65c826ea43abe101fd4be
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-08 23:43:09 +0900

    setup enju_inventory

--- lib/generators/enju_leaf/quick_install/quick_install_generator.rb
+++ lib/generators/enju_leaf/quick_install/quick_install_generator.rb
@@ -37,12 +37,14 @@ EOS
     rake("enju_biblio_engine:install:migrations")
     rake("enju_manifestation_viewer_engine:install:migrations")
     rake("enju_subject_engine:install:migrations")
+    rake("enju_inventory_engine:install:migrations")
     if !ENV['ENJU_SKIP_CONFIG']
       generate("enju_seed:setup")
       generate("enju_library:setup")
       generate("enju_biblio:setup")
       generate("enju_circulation:setup")
       generate("enju_subject:setup")
+      generate("enju_inventory:setup")
     end
     rake("enju_ndl_engine:install:migrations")
     append_to_file "app/models/user.rb", <<EOS.strip_heredoc
