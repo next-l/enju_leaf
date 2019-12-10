8864119e8ba5d0f91c952afbd0d479785f07596a
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-10 19:53:45 +0900

    update webpacker files

--- lib/enju_leaf.rb
+++ lib/enju_leaf.rb
@@ -5,21 +5,6 @@ require 'nkf'
 require 'ipaddr'
 
 module EnjuLeaf
-  if Rails.env == 'test'
-    ROOT_PATH = Pathname.new(File.join(__dir__, "../spec/dummy"))
-  else
-    ROOT_PATH = Pathname.new(File.join(__dir__, ".."))
-  end
-
-  class << self
-    def webpacker
-      @webpacker ||= ::Webpacker::Instance.new(
-        root_path: ROOT_PATH,
-        config_path: ROOT_PATH.join("config/webpacker.yml")
-      )
-    end
-  end
-
   class InvalidLocaleError < StandardError
   end
 end
