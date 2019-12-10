32c8f9d3ab72c75f0c40f293685f65e489eac142
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-10 22:00:35 +0900

    use rails/ujs and rails/activestorage

--- spec/dummy/app/javascript/packs/application.js
+++ spec/dummy/app/javascript/packs/application.js
@@ -1,2 +1,10 @@
 import 'bootstrap'
 import '../src/application.scss'
+
+import Rails from '@rails/ujs'
+Rails.start()
+
+import * as ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
+
+
