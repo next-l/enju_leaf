70bd03194c91e39b790992bdfa17b251c50f7a6c
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-21 20:28:19 +0900

    install enju_inventory next-l/enju_leaf#1455

--- lib/generators/enju_leaf/quick_install/quick_install_generator.rb
+++ lib/generators/enju_leaf/quick_install/quick_install_generator.rb
@@ -10,12 +10,14 @@ class EnjuLeaf::QuickInstallGenerator < Rails::Generators::Base
     rake("enju_biblio_engine:install:migrations")
     rake("enju_manifestation_viewer_engine:install:migrations")
     rake("enju_subject_engine:install:migrations")
+    rake("enju_inventory_engine:install:migrations")
     if !ENV['ENJU_SKIP_CONFIG']
       generate("enju_seed:setup")
       generate("enju_library:setup")
       generate("enju_biblio:setup")
       generate("enju_circulation:setup")
       generate("enju_subject:setup")
+      generate("enju_inventory:setup")
     end
   end
 end
