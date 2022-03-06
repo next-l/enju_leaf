aca3c42c402e73bc450fc7a2ebfe9a958811da52
Kosuke Tanabe <nabeta@fastmail.fm>
2022-03-06 19:33:28 +0900

    text -> txt

---
+++ spec/system/checkouts_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Checkouts', type: :system do
+  include Devise::Test::IntegrationHelpers
+  fixtures :all
+
+  before(:each) do
+    CarrierType.find_by(name: 'volume').update(attachment: File.open("#{Rails.root.to_s}/app/assets/images/icons/book.png"))
+  end
+
+  describe 'When logged in as Librarian' do
+    it 'should contain query params in the facet' do
+      sign_in users(:librarian1)
+      visit checkout_path(checkouts(:checkout_00001))
+      expect(page).to have_content '利用者番号'
+      expect(page).to have_content checkouts(:checkout_00001).user.username
+      expect(page).to have_content checkouts(:checkout_00001).user.profile.user_number
+    end
+  end
+
+  describe 'When not logged in', solr: true do
+    before(:each) do
+      Checkout.reindex
+    end
+
+    it 'should contain query params in the facet' do
+      sign_in users(:librarian1)
+      visit checkouts_path(days_overdue: 10)
+      expect(page).to have_link 'RSS', href: checkouts_path(format: :rss, days_overdue: 10)
+      expect(page).to have_link 'TSV', href: checkouts_path(format: :txt, days_overdue: 10, locale: 'ja')
+    end
+  end
+end
