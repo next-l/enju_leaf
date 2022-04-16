b87f0ef4f291874d3b4091231c1b71aa568ce97c
Kosuke Tanabe <nabeta@fastmail.fm>
2022-04-16 22:27:37 +0900

    remove some actions from RequestStatusType and RequestType

--- app/controllers/request_types_controller.rb
+++ app/controllers/request_types_controller.rb
@@ -1,6 +1,6 @@
 class RequestTypesController < ApplicationController
-  before_action :set_request_type, only: [:show, :edit, :update, :destroy]
-  before_action :check_policy, only: [:index, :new, :create]
+  before_action :set_request_type, only: [:show, :edit, :update]
+  before_action :check_policy, only: [:index]
 
   # GET /request_types
   # GET /request_types.json
@@ -22,35 +22,8 @@ class RequestTypesController < ApplicationController
     end
   end
 
-  # GET /request_types/new
-  # GET /request_types/new.json
-  def new
-    @request_type = RequestType.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @request_type }
-    end
-  end
-
   # GET /request_types/1/edit
   def edit
-  end
-
-  # POST /request_types
-  # POST /request_types.json
-  def create
-    @request_type = RequestType.new(request_type_params)
-
-    respond_to do |format|
-      if @request_type.save
-        format.html { redirect_to @request_type, notice: t('controller.successfully_created', model: t('activerecord.models.request_type')) }
-        format.json { render json: @request_type, status: :created, location: @request_type }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @request_type.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PUT /request_types/1
@@ -69,17 +42,6 @@ class RequestTypesController < ApplicationController
         format.html { render action: "edit" }
         format.json { render json: @request_type.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /request_types/1
-  # DELETE /request_types/1.json
-  def destroy
-    @request_type.destroy
-
-    respond_to do |format|
-      format.html { redirect_to request_types_url, notice: t('controller.successfully_deleted', model: t('activerecord.models.request_type')) }
-      format.json { head :no_content }
     end
   end
 
