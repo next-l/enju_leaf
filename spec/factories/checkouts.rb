d4e7e6d3cd13f069cdfe2ec350bc311fbab12f5f
Kosuke Tanabe <nabeta@fastmail.fm>
2022-04-17 17:47:14 +0900

    fix invalid migration file

--- spec/factories/checkouts.rb
+++ spec/factories/checkouts.rb
@@ -7,3 +7,22 @@ FactoryBot.define do
     f.association :basket, factory: :basket
   end
 end
+
+# == Schema Information
+#
+# Table name: checkouts
+#
+#  id                     :integer          not null, primary key
+#  user_id                :integer
+#  item_id                :integer          not null
+#  checkin_id             :integer
+#  librarian_id           :integer
+#  basket_id              :integer
+#  due_date               :datetime
+#  checkout_renewal_count :integer          default(0), not null
+#  lock_version           :integer          default(0), not null
+#  created_at             :datetime
+#  updated_at             :datetime
+#  shelf_id               :integer
+#  library_id             :integer
+#
