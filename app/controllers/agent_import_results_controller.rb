fac259009a7971905cfcab74fe6b461ba5d1e29f
Kosuke Tanabe <nabeta@fastmail.fm>
2022-04-21 02:56:48 +0900

    fix checking agent_import_file

--- app/controllers/agent_import_results_controller.rb
+++ app/controllers/agent_import_results_controller.rb
@@ -6,9 +6,9 @@ class AgentImportResultsController < ApplicationController
   # GET /agent_import_results.json
   def index
     @agent_import_file = AgentImportFile.find_by(id: params[:agent_import_file_id])
-    authorize @agent_import_file, :show?, policy_class: AgentImportFilePolicy
 
     if @agent_import_file
+      authorize @agent_import_file, :show?, policy_class: AgentImportFilePolicy
       @agent_import_results = @agent_import_file.agent_import_results.page(params[:page])
     else
       @agent_import_results = AgentImportResult.page(params[:page])
