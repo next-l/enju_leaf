8a75342a7fed13b48868533fc99da573c090e9fc
Kosuke Tanabe <nabeta@fastmail.fm>
2022-04-23 15:10:17 +0900

    remove DemandsController and PlacesController

---
+++ spec/factories/demands.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :demand do
+    user_id { FactoryBot.create(:user).id }
+    item_id { FactoryBot.create(:item).id }
+    message_id { FactoryBot.create(:message).id }
+  end
+end
+
+# == Schema Information
+#
+# Table name: demands
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  item_id    :integer
+#  message_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -3,3 +3,14 @@ FactoryBot.define do
     f.sequence(:name){|n| "tag_#{n}"}
   end
 end
+
+# == Schema Information
+#
+# Table name: tags
+#
+#  id             :integer          not null, primary key
+#  name           :string
+#  taggings_count :integer          default(0)
+#  created_at     :datetime
+#  updated_at     :datetime
+#
