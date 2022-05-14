a13745e3e1ea488332aa22142076d95b19c65be2
Kosuke Tanabe <nabeta@fastmail.fm>
2022-05-14 23:39:31 +0900

    ENJU_LEAF_DEFAULT_LOCALEとENJU_LEAF_DEFAULT_TIME_ZONEを追加 (#1673)
    * add ENJU_LEAF_DEFAULT_LOCALE and ENJU_LEAF_DEFAULT_TIME_ZONE
    
    * fix test environment variables

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module EnjuLeaf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = (ENV['ENJU_LEAF_DEFAULT_LOCALE'] || 'en').to_sym
+    config.time_zone = ENV['ENJU_LEAF_DEFAULT_TIME_ZONE'] || 'UTC'
   end
 end
