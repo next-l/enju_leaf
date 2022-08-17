d496ba0355354270d0110f8487f8f58ae2b12979
Kosuke Tanabe <nabeta@fastmail.fm>
2022-08-17 13:36:50 +0900

    update spec file

--- spec/helpers/manifestations_helper_spec.rb
+++ spec/helpers/manifestations_helper_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe ManifestationsHelper do
   fixtures :all
 
+  it 'should export rdf_statement"' do
+    expect(rdf_statement(manifestations(:manifestation_00001)).to_s).to match '<https://next-l.jp/vocab/originalTitle> "よくわかる最新Webサービス技術の基本と仕組み : 標準Webシステム技術とeコマース基盤技術入門"'
+  end
+
   it "should get paginate_id_link" do
     helper.paginate_id_link(manifestations(:manifestation_00003), [1, 2, 3]).should =~ /Next <a href=\"\/manifestations\/2\">Previous<\/a>/
   end
