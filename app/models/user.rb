fe1e45e4d0a895e7ec5debceff4055e6504f59b6
Kosuke Tanabe <nabeta@fastmail.fm>
2022-08-23 02:51:35 +0900

    update annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,18 +16,24 @@ end
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  username               :string
-#  expired_at             :datetime
-#  failed_attempts        :integer          default(0)
-#  unlock_token           :string
-#  locked_at              :datetime
-#  confirmed_at           :datetime
+#  id                        :bigint           not null, primary key
+#  email                     :string           default(""), not null
+#  encrypted_password        :string           default(""), not null
+#  reset_password_token      :string
+#  reset_password_sent_at    :datetime
+#  remember_created_at       :datetime
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  username                  :string
+#  expired_at                :datetime
+#  failed_attempts           :integer          default(0)
+#  unlock_token              :string
+#  locked_at                 :datetime
+#  confirmed_at              :datetime
+#  encrypted_otp_secret      :string
+#  encrypted_otp_secret_iv   :string
+#  encrypted_otp_secret_salt :string
+#  consumed_timestep         :integer
+#  otp_required_for_login    :boolean          default(FALSE), not null
+#  otp_backup_codes          :string           is an Array
 #
