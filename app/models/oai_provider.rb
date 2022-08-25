669716906ae67521ab8c446519591c952152dc99
Kosuke Tanabe <nabeta@fastmail.fm>
2022-08-26 02:43:35 +0900

    update record_prefix

--- app/models/oai_provider.rb
+++ app/models/oai_provider.rb
@@ -2,7 +2,7 @@ class OaiProvider < OAI::Provider::Base
   library_group = LibraryGroup.site_config
   repository_name library_group.display_name
   repository_url URI.join(ENV['ENJU_LEAF_BASE_URL'], '/oai')
-  record_prefix "oai:#{library_group.name}:manifestations"
+  record_prefix "oai:#{URI.parse(ENV['ENJU_LEAF_BASE_URL']).host}:manifestations"
   admin_email library_group.email
   source_model OAI::Provider::ActiveRecordWrapper.new(
     Manifestation.where(required_role: 1)
