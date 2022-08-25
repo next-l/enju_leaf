f12f13477a63c1d4a6acacfe1064b3f1c07e1971
Kosuke Tanabe <nabeta@fastmail.fm>
2022-08-26 08:55:50 +0900

    add namespace to Dcndl

--- app/models/concerns/enju_oai/dcndl.rb
+++ app/models/concerns/enju_oai/dcndl.rb
@@ -3,6 +3,8 @@ module EnjuOai
 
     def initialize
       @prefix = 'dcndl'
+      @schema = nil
+      @namespace = 'http://ndl.go.jp/dcndl/terms/'
       @element_namespace = 'dc'
       @fields = [ :title, :creator, :subject, :description, :publisher,
                   :contributor, :date, :type, :format, :identifier,
