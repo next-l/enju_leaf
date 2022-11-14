7317d913063fe3d97ac2f3c02519a816f13a8f68
Kosuke Tanabe <nabeta@fastmail.fm>
2022-11-14 21:26:23 +0900

    skip changing timestamp columns in Active Storage tables

--- db/migrate/20221112015501_add_not_null_on_timestamps.rb
+++ db/migrate/20221112015501_add_not_null_on_timestamps.rb
@@ -1,12 +1,12 @@
 class AddNotNullOnTimestamps < ActiveRecord::Migration[6.1]
   def change
     ActiveRecord::Base.connection.tables.each do |table|
-      next if ['schema_migrations', 'countries', 'languages', 'active_storage_variant_records'].include?(table)
+      next if ['schema_migrations', 'countries', 'languages', 'active_storage_blobs', 'active_storage_attachments', 'active_storage_variant_records'].include?(table)
 
       change_column_null table, :created_at, false
       change_column table, :created_at, :datetime, precision: 6
 
-      next if ['taggings', 'active_storage_blobs', 'active_storage_attachments', 'active_storage_variant_records'].include?(table)
+      next if 'taggings' == table
 
       change_column_null table, :updated_at, false
       change_column table, :updated_at, :datetime, precision: 6
