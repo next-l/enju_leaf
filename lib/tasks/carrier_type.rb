72f98aa7af656c011d831b3ac20de1e171ec7ecf
Kosuke Tanabe <nabeta@fastmail.fm>
2022-11-21 22:42:00 +0900

    fix attaching default icons

--- lib/tasks/carrier_type.rb
+++ lib/tasks/carrier_type.rb
@@ -4,32 +4,32 @@ def update_carrier_type
     when "volume"
       carrier_type = CarrierType.find_by(name: 'volume')
       if carrier_type
-        unless carrier_type.attachment_file_name
-          carrier_type.attachment = File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/book.png")
+        unless carrier_type.attachment.attached?
+          carrier_type.attachment.attach(io: File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/book.png"), filename: 'book.png')
           carrier_type.save!
         end
       end
     when "audio_disc"
       carrier_type = CarrierType.find_by(name: 'audio_disc')
       if carrier_type
-        unless carrier_type.attachment_file_name
-          carrier_type.attachment = File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/cd.png")
+        unless carrier_type.attachment.attached?
+          carrier_type.attachment.attach(io: File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/cd.png"), filename: 'cd.png')
           carrier_type.save!
         end
       end
     when "videodisc"
       carrier_type = CarrierType.find_by(name: 'videodisc')
       if carrier_type
-        unless carrier_type.attachment_file_name
-          carrier_type.attachment = File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/dvd.png")
+        unless carrier_type.attachment.attached?
+          carrier_type.attachment.attach(io: File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/dvd.png"), filename: 'dvd.png')
           carrier_type.save!
         end
       end
     when "online_resource"
       carrier_type = CarrierType.find_by(name: 'online_resource')
       if carrier_type
-        unless carrier_type.attachment_file_name
-          carrier_type.attachment = File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/monitor.png")
+        unless carrier_type.attachment.attached?
+          carrier_type.attachment.attach(io: File.open("#{File.dirname(__FILE__)}/../../app/assets/images/icons/monitor.png"), filename: 'monitor.png')
           carrier_type.save!
         end
       end
