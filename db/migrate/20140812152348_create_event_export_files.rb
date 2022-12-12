9892323eca567511aac237887bb418a63680b6e4
Kosuke Tanabe <nabeta@fastmail.fm>
2022-12-13 00:03:49 +0900

    remove paperclip migrations

--- db/migrate/20140812152348_create_event_export_files.rb
+++ db/migrate/20140812152348_create_event_export_files.rb
@@ -2,7 +2,6 @@ class CreateEventExportFiles < ActiveRecord::Migration[4.2]
   def change
     create_table :event_export_files do |t|
       t.references :user, index: true
-      t.attachment :event_export
       t.datetime :executed_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_155729) do
+ActiveRecord::Schema.define(version: 2022_11_14_163530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
