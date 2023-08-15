47b6a04cdda08a0df2a604d6dcd0c5bd08c86a6f
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-15 20:53:56 +0900

    update column name

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -99,7 +99,7 @@ class UserGroupsController < ApplicationController
       # EnjuCirculation
       {user_group_has_checkout_types_attributes: [
         :id, :checkout_type_id, :checkout_limit, :checkout_period, :checkout_renewal_limit,
-        :reservation_limit, :reservation_expired_period, :set_due_date_before_closing_day
+        :reservation_limit, :reservation_expired_period, :due_date_offset
       ]}
     )
   end
