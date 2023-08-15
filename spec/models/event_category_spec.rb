bd379552b4790f16ddcdac9cef16bbc67e17971d
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-15 21:01:02 +0900

    add spec files

---
+++ spec/models/event_category_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe EventCategory do
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+end
+
+# == Schema Information
+#
+# Table name: event_categories
+#
+#  id                        :bigint           not null, primary key
+#  name                      :string           not null
+#  note                      :text
+#  position                  :integer
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  display_name_translations :jsonb            not null
+#
