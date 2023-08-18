92e770d67d62edf880285707c1d077d8e05f0cc2
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-18 14:32:38 +0900

    fix template

--- spec/views/periodicals/index.html.erb_spec.rb
+++ spec/views/periodicals/index.html.erb_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "periodicals/index", type: :view do
   end
 
   it "renders a list of periodicals" do
+    allow(view).to receive(:policy).and_return double(update?: true, destroy?: true)
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
   end
