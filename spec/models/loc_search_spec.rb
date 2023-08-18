724d55dcd1c57acc36d2ee5c20efd282e1016bdb
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-19 01:17:30 +0900

    fix spec files

--- spec/models/loc_search_spec.rb
+++ spec/models/loc_search_spec.rb
@@ -41,7 +41,7 @@ describe LocSearch do
       expect(classification.classification_type.name).to eq "ddc"
       expect(classification.category).to eq "303.48"
       expect(manifestation.isbn_records.first.body).to eq "9780805080438"
-      expect(manifestation.identifier_contents("lccn").first).to eq "2007012024"
+      expect(manifestation.lccn_record.body).to eq "2007012024"
     end
 
     it "should parse title information properly", vcr: true do
