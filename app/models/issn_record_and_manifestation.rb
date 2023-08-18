28cae908512643242c6c503619a149c3334a77d7
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-18 23:25:10 +0900

    add IssnRecord and IssnRecordAndManifestation

---
+++ app/models/issn_record_and_manifestation.rb
@@ -0,0 +1,16 @@
+class IssnRecordAndManifestation < ApplicationRecord
+  belongs_to :issn_record
+  belongs_to :manifestation
+end
+
+# == Schema Information
+#
+# Table name: issn_record_and_manifestations
+#
+#  id               :bigint           not null, primary key
+#  issn_record_id   :bigint           not null
+#  manifestation_id :bigint           not null
+#  position         :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
