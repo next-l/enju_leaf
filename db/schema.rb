658167de448738fa26765127a15af3adee2d1381
Kosuke Tanabe <nabeta@fastmail.fm>
2023-08-19 01:28:58 +0900

    rename migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_18_052023) do
+ActiveRecord::Schema.define(version: 2022_11_14_163530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
