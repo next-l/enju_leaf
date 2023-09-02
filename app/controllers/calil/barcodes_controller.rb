d645c886dfb4640e89eece0d93f830df8ef02260
kimihito <tatsurotamashiro@gmail.com>
2023-09-02 22:57:48 +0900

    wip: カーリル連番印刷の作成フォームを追加

--- app/controllers/calil/barcodes_controller.rb
+++ app/controllers/calil/barcodes_controller.rb
@@ -1,5 +1,9 @@
 class Calil::BarcodesController < ApplicationController
   def new
+    @library = Library.friendly.find(params[:library_id])
+    authorize @library
+
+    @calil_barcode = Calil::Barcode.new(name: @library.name)
   end
 
   def create
