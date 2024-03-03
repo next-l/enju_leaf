c802d9f956a1a9e47bd229e450199495665bf08a
Kosuke Tanabe <nabeta@fastmail.fm>
2024-03-04 00:51:22 +0900

    pub_dateとdate_of_publicationの両方をエクスポートするよう変更 (#1867)
    * export both pub_date and date_of_publication
    
    * fix exporting pub_date
    
    * fix typo
    
    * fix spec file
    
    * update spec files

--- app/models/manifestation.rb
+++ app/models/manifestation.rb
@@ -585,7 +585,8 @@ class Manifestation < ApplicationRecord
           "#{produce.agent.full_name}"
         end
       }.join('//'),
-      pub_date: date_of_publication,
+      pub_date: pub_date,
+      date_of_publication: date_of_publication,
       year_of_publication: year_of_publication,
       publication_place: publication_place,
       manifestation_created_at: created_at,
