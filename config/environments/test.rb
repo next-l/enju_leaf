3af38d9453ce07383b9afef52025ee6f1e0719e0
Kosuke Tanabe <nabeta@fastmail.fm>
2024-08-16 23:11:05 +0900

    update queue adapter

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.active_job.queue_adapter = :inline
 end
