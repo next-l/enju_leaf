2285e3b019d372dfd3b7be719a240494f1a2a479
Kosuke Tanabe <nabeta@fastmail.fm>
2024-08-17 22:36:52 +0900

    remove rails-ujs from app/javascript/packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,9 +17,4 @@
 
 //console.log('Hello World from Webpacker')
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-Rails.start()
-ActiveStorage.start()
