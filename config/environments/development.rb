1390d580a2117053a56bdb7c80539d13ce01db1a
Kosuke Tanabe <nabeta@fastmail.fm>
2024-08-17 20:49:01 +0900

    add MissionControlController

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.active_job.queue_adapter     = :solid_queue
+  config.active_job.queue_name_prefix = "enju_rdm_development"
 end
