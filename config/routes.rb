661698030106dcd838a33e279b91b59997fa3a4d
Kosuke Tanabe <nabeta@fastmail.fm>
2024-08-19 23:04:59 +0900

    enable health check endpoint (#1901)

--- config/routes.rb
+++ config/routes.rb
@@ -166,6 +166,13 @@ Rails.application.routes.draw do
   get '/page/system_information' => 'page#system_information'
   get '/page/routing_error' => 'page#routing_error'
   match 'oai', to: "oai#index", via: [:get, :post]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
   root :to => "page#index"
 end
