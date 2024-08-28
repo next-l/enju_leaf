05372034bd925ea00b2ec567fd515040aa8a4e62
Kosuke Tanabe <nabeta@fastmail.fm>
2024-08-29 00:15:06 +0900

    remove config.hosts from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,4 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.active_job.queue_adapter = :solid_queue
-
-  config.hosts += ['web', '127.0.0.1', 'localhost']
 end
