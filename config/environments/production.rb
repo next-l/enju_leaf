98bdaf916720c2ad904cfc5c18f860c6319559f5
Kosuke Tanabe <nabeta@fastmail.fm>
2024-12-23 22:47:30 +0900

    disable css compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.assets.css_compressor = nil
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
