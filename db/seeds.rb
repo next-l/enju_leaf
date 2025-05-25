38aa1502a140d6bad1c97f89119e3a14ed03a7fc
Kosuke Tanabe <nabeta@fastmail.fm>
2025-05-25 15:26:26 +0900

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,33 +19,37 @@ end
 
 Rake::Task['enju_leaf:setup'].invoke
 
-system_user = User.new
-system_user.username = 'system'
-system_user.password = SecureRandom.urlsafe_base64(32)
-system_user.email = 'root@library.example.jp'
-system_user.role = Role.find_by(name: 'Administrator')
-profile = new_profile
-profile.save!
-system_user.profile = profile
-system_user.save!
-LibraryGroup.first.update!(
-  email: system_user.email,
-  url: ENV['ENJU_LEAF_BASE_URL']
-)
-UserGroup.order(created_at: :desc).first.update!(
-  number_of_day_to_notify_overdue: 7,
-  number_of_day_to_notify_due_date: 3
-)
+if User.count.zero?
+  system_user = User.new(
+    username: 'system',
+    email: 'root@library.example.jp',
+    role: Role.find_by(name: 'Administrator')
+  )
+  system_user.password = SecureRandom.urlsafe_base64(32)
+  profile = new_profile
+  profile.save!
+  system_user.profile = profile
+  system_user.save!
+  LibraryGroup.first.update!(
+    email: system_user.email,
+    url: ENV['ENJU_LEAF_BASE_URL']
+  )
+  UserGroup.order(created_at: :desc).first.update!(
+    number_of_day_to_notify_overdue: 7,
+    number_of_day_to_notify_due_date: 3
+  )
 
-user = User.new
-user.username = username
-user.email = email
-user.password = password
-user.password_confirmation = password
-user.role = Role.find_by(name: 'Administrator')
-profile = new_profile
-profile.user_number = '0'
-profile.save!
-user.profile = profile
-user.save!
-puts 'Administrator account created.'
+  user = User.new(
+    username: username,
+    email: email
+    role: Role.find_by(name: 'Administrator')
+  )
+  user.password = password
+  user.password_confirmation = password
+  profile = new_profile
+  profile.user_number = '0'
+  profile.save!
+  user.profile = profile
+  user.save!
+  puts 'Administrator account created.'
+end
