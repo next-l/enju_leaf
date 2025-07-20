610ed2b682cc22d722378977671b27de6c1df0c3
Kosuke Tanabe <nabeta@fastmail.fm>
2025-07-20 13:28:14 +0900

    remove paperclip columns

--- db/migrate/20171014084528_add_header_logo_to_library_group.rb
+++ db/migrate/20171014084528_add_header_logo_to_library_group.rb
@@ -1,5 +1,5 @@
 class AddHeaderLogoToLibraryGroup < ActiveRecord::Migration[4.2]
   def change
-    add_attachment :library_groups, :header_logo
+    # add_attachment :library_groups, :header_logo
   end
 end
