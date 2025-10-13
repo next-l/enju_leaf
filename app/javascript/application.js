728c7d5af1ecb0cc90a6aca56f288413a0cd7975
Kosuke Tanabe <nabeta@fastmail.fm>
2025-10-14 02:44:29 +0900

    add tom-select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
+import "@popperjs/core"
 import "bootstrap"
 import "@oddcamp/cocoon-vanilla-js"
+import tom_select from "tom-select"
+window.TomSelect = tom_select
