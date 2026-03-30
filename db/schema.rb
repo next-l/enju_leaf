95b1659b5be4ab5c3de802b00b83ad84c07e0243
Kosuke Tanabe <nabeta@fastmail.fm>
2026-03-31 02:17:09 +0900

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2025_07_26_140939) do
-=======
 ActiveRecord::Schema[7.2].define(version: 2025_07_27_021342) do
->>>>>>> main
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
