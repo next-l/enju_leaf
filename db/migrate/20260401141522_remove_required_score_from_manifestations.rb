7131c77636340b2024724e4c83483e1d600937c0
Kosuke Tanabe <nabeta@fastmail.fm>
2026-04-01 23:20:54 +0900

    remove required_score column from agents and items tables

--- db/migrate/20260401141522_remove_required_score_from_manifestations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRequiredScoreFromManifestations < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :manifestations, :required_score, :integer, default: 0, null: false
-  end
-end
