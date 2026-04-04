cb02169b7f5a260ffd31a36854c1b609c2fa25f7
Kosuke Tanabe <nabeta@fastmail.fm>
2026-04-05 02:14:04 +0900

    fix creating new user and profile in seeds.rb (#2012)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   include EnjuMessage::EnjuUser
   include EnjuBookmark::EnjuUser
   include EnjuPurchaseRequest::EnjuUser
+  belongs_to :profile
   validates :profile, uniqueness: true
 end
 
