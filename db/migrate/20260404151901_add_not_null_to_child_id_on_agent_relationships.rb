5235606dfd2adacf553c10dd2e7158a0a914e7b2
Kosuke Tanabe <nabeta@fastmail.fm>
2026-04-05 03:00:01 +0900

    set default id

--- db/migrate/20260404151901_add_not_null_to_child_id_on_agent_relationships.rb
+++ db/migrate/20260404151901_add_not_null_to_child_id_on_agent_relationships.rb
@@ -4,9 +4,15 @@ class AddNotNullToChildIdOnAgentRelationships < ActiveRecord::Migration[7.2]
       agent_relationship.destroy unless agent_relationship.child
       agent_relationship.destroy unless agent_relationship.parent
     end
+
+    if AgentRelationshipType.count.positive? && AgentRelationshipType.find_by(id: 1).nil?
+      AgentRelationshipType.order(:position).first.update_column(:id, 1)
+    end
+
     AgentRelationship.where(agent_relationship_type_id: nil).find_each do |agent_relationship|
       agent_relationship.update_column(:agent_relationship_type_id, 1)
     end
+
     change_column_null :agent_relationships, :child_id, false
     change_column_null :agent_relationships, :parent_id, false
     change_column_null :agent_relationships, :agent_relationship_type_id, false
@@ -19,9 +25,15 @@ class AddNotNullToChildIdOnAgentRelationships < ActiveRecord::Migration[7.2]
       manifestation_relationship.destroy unless manifestation_relationship.child
       manifestation_relationship.destroy unless manifestation_relationship.parent
     end
+
+    if ManifestationRelationshipType.count.positive? && ManifestationRelationshipType.find_by(id: 1).nil?
+      ManifestationRelationshipType.order(:position).first.update_column(:id, 1)
+    end
+
     ManifestationRelationship.where(manifestation_relationship_type_id: nil).find_each do |manifestation_relationship|
       manifestation_relationship.update_column(:manifestation_relationship_type_id, 1)
     end
+
     change_column_null :manifestation_relationships, :child_id, false
     change_column_null :manifestation_relationships, :parent_id, false
     change_column_null :manifestation_relationships, :manifestation_relationship_type_id, false
