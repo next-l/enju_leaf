a71de55f18d4c1b5e1019b8a05fca2077686fd6a
Kosuke Tanabe <nabeta@fastmail.fm>
2026-04-06 02:48:01 +0900

    library_idカラムに外部キー制約を追加 (#2026)
    * add foreign key to library_id column
    
    * add foreign key to library_id column
    
    * fix spec file
    
    * fix spec file

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -193,15 +193,6 @@ describe ItemsController do
         expect(response).to redirect_to(manifestations_url)
       end
 
-      it 'should work without exception, even if library and shelf is unavailable' do
-        Library.real.each do |library|
-          library.try(:shelves).to_a.each(&:destroy)
-          library.delete
-        end
-        get :new, params: { manifestation_id: @manifestation.id }
-        expect(response).to redirect_to(libraries_url)
-      end
-
       it 'should not get new item for series_master' do
         manifestation_serial = FactoryBot.create(:manifestation_serial)
         get :new, params: { manifestation_id: manifestation_serial.id }
