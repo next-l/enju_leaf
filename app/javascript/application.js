38e0369f1347dbde7c304eb0ba50bcfa67466b32
Kosuke Tanabe <nabeta@fastmail.fm>
2026-05-17 14:55:27 +0900

    disable Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 import "@popperjs/core"
