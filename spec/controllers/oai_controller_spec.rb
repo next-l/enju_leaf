4391bf1c359dfd2f8f1ead15c785306703ac9c77
Kosuke Tanabe <nabeta@fastmail.fm>
2026-05-19 00:25:29 +0900

    fix spec file

--- spec/controllers/oai_controller_spec.rb
+++ spec/controllers/oai_controller_spec.rb
@@ -3,51 +3,25 @@ require 'rails_helper'
 RSpec.describe OaiController, type: :controller do
   fixtures :all
 
-  def valid_attributes
-    FactoryBot.attributes_for(:manifestation)
-  end
-
   describe 'GET index', solr: true do
-    before do
-      50.times do
-        FactoryBot.create(:manifestation)
-      end
-      Manifestation.reindex
-    end
-
     describe 'When not logged in' do
       it 'assigns all manifestations as @manifestations in oai format without verb' do
-        get :provider, params: { format: 'xml' }
+        get :index, params: { metadataPrefix: 'oai_dc' }
         assigns(:manifestations).should_not be_nil
-        response.should render_template('oai/provider')
       end
 
       it 'should not assign all manifestations as @manifestations in oai format with ListRecords without metadataPrefix' do
-        get :provider, params: { format: 'xml', verb: 'ListRecords' }
+        get :index, params: { verb: 'ListRecords' }
         assigns(:manifestations).should_not be_nil
-        response.should render_template('oai/provider')
-      end
-
-      it 'assigns all manifestations as @manifestations in oai format with ListRecords for junii2 metadata' do
-        get :provider, params: { format: 'xml', verb: 'ListRecords', metadataPrefix: 'junii2' }
-        assigns(:manifestations).should_not be_nil
-        response.should render_template('oai/list_records')
       end
 
       it 'should not assign all manifestations as @manifestations in oai format with ListIdentifiers without metadataPrefix' do
-        get :provider, params: { format: 'xml', verb: 'ListIdentifiers' }
+        get :index, params: { verb: 'ListIdentifiers' }
         assigns(:manifestations).should_not be_nil
-        response.should render_template('oai/provider')
-      end
-
-      it 'assigns all manifestations as @manifestations in oai format with ListIdentifiers for junii2 metadata' do
-        get :provider, params: { format: 'xml', verb: 'ListIdentifiers', metadataPrefix: 'junii2' }
-        assigns(:manifestations).should_not be_nil
-        response.should render_template('oai/list_identifiers')
       end
 
       it 'assigns all manifestations as @manifestations in oai format with GetRecord without identifier' do
-        get :provider, params: { format: 'xml', verb: 'GetRecord' }
+        get :index, params: { verb: 'GetRecord', metadataFormat: 'oai_dc' }
         assigns(:manifestations).should be_nil
         assigns(:manifestation).should be_nil
         response.should render_template('oai/provider')
@@ -55,40 +29,19 @@ RSpec.describe OaiController, type: :controller do
 
       it 'should not assign all manifestations as @manifestations in oai format with GetRecord with identifier without metadataPrefix' do
         manifestation = Manifestation.first
-        get :provider, params: { format: 'xml', verb: 'GetRecord', identifier: "oai:localhost:manifestations-#{manifestation.id}" }
+        get :index, params: { verb: 'GetRecord', identifier: "oai:localhost:manifestations:#{manifestation.id}", metadataFormat: 'oai_dc' }
         assigns(:manifestations).should be_nil
         assigns(:manifestation).should_not be_nil
-        response.should render_template('oai/provider')
       end
 
-      it 'assigns all manifestations as @manifestations in oai format with GetRecord with identifier for junii2 metadata' do
-        manifestation = Manifestation.first
-        get :provider, params: { format: 'xml', verb: 'GetRecord', identifier: "oai:localhost:manifestations-#{manifestation.id}", metadataPrefix: 'junii2' }
-        assigns(:manifestations).should be_nil
-        assigns(:manifestation).should_not be_nil
-        response.should render_template('oai/get_record')
-      end
-
-      it "should return only public identifiers for oai_dc metadata" do
-        get :provider, params: { format: 'xml', verb: 'ListIdentifiers', metadataPrefix: 'oai_dc' }
+      it "should return only public identifiers for jpcoar_2.0 metadata" do
+        get :index, params: { verb: 'ListIdentifiers', metadataPrefix: 'jpcoar_2.0' }
         expect(assigns(:manifestations).map(&:id).include?(11)).to be_falsy
         expect(assigns(:manifestations).map(&:id).include?(24)).to be_falsy
       end
 
-      it "should return only public identifiers for junii2 metadata" do
-        get :provider, params: { format: 'xml', verb: 'ListIdentifiers', metadataPrefix: 'junii2' }
-        expect(assigns(:manifestations).map(&:id).include?(11)).to be_falsy
-        expect(assigns(:manifestations).map(&:id).include?(24)).to be_falsy
-      end
-
-      it "should return only public records for oai_dc metadata" do
-        get :provider, params: { format: 'xml', verb: 'ListRecords', metadataPrefix: 'oai_dc' }
-        expect(assigns(:manifestations).map(&:id).include?(11)).to be_falsy
-        expect(assigns(:manifestations).map(&:id).include?(24)).to be_falsy
-      end
-
-      it "should return only public records for junii2 metadata" do
-        get :provider, params: { format: 'xml', verb: 'ListRecords', metadataPrefix: 'junii2' }
+      it "should return only public records for jpcoar_2.0 metadata" do
+        get :index, params: { verb: 'ListRecords', metadataPrefix: 'jpcoar_2.0' }
         expect(assigns(:manifestations).map(&:id).include?(11)).to be_falsy
         expect(assigns(:manifestations).map(&:id).include?(24)).to be_falsy
       end
