de62d65d20df00f9174391396397bea3a27f7305
Kosuke Tanabe <nabeta@fastmail.fm>
2026-08-08 20:57:04 +0900

    fix tests

--- spec/controllers/oai_controller_spec.rb
+++ spec/controllers/oai_controller_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 describe OaiController do
   fixtures :all
 
+  before(:each) do
+    manifestatons(:manifestation_00001).attach(
+      io: File.open(Rails.root.join('README.md')),
+      filename: 'README.md'
+    )
+  end
+
   describe "GET identify" do
     it "should get identify" do
-      get oai_path, verb: "Identify"
+      get oai_path(verb: "Identify")
       expect(response).to be_successful
     end
 
     it "should get list_records" do
-      get oai_path, verb: "ListRecords", metadataPrefix: "jpcoar_2.0"
+      get oai_path(verb: "ListRecords", metadataPrefix: "jpcoar_2.0")
       expect(response).to be_successful
     end
   end
