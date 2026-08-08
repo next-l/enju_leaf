a8ce7cfa66b2f2750c78577107b590b059006060
Kosuke Tanabe <nabeta@fastmail.fm>
2026-08-09 01:29:38 +0900

    EnjuSubjectモジュールを削除 (#2047)
    * remove EnjuSubject module
    
    * fix CI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < ActionController::Base
   include EnjuLibrary::Controller
   include EnjuBiblio::Controller
   include EnjuEvent::Controller
-  include EnjuSubject::Controller
   include Pundit::Authorization
   after_action :verify_authorized, unless: :devise_controller?
   impersonates :user
+
+  def get_subject_heading_type
+    if params[:subject_heading_type_id]
+      @subject_heading_type = SubjectHeadingType.find(params[:subject_heading_type_id])
+      authorize @subject_heading_type, :show?
+    end
+  end
+
+  def get_subject
+    if params[:subject_id]
+      @subject = Subject.find(params[:subject_id])
+      authorize @subject, :show?
+    end
+  end
+
+  def get_classification
+    if params[:classification_id]
+      @classification = Classification.find(params[:classification_id])
+      authorize @classification, :show?
+    end
+  end
 end
