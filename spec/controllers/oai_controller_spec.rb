f6ca630144d793abe65a96a6e407b9b14d0f5773
Kosuke Tanabe <nabeta@fastmail.fm>
2026-08-08 21:10:35 +0900

    fix spell

--- spec/controllers/oai_controller_spec.rb
+++ spec/controllers/oai_controller_spec.rb
@@ -4,7 +4,7 @@ describe OaiController do
   fixtures :all
 
   before(:each) do
-    manifestatons(:manifestation_00001).attach(
+    manifestations(:manifestation_00001).attach(
       io: File.open(Rails.root.join('README.md')),
       filename: 'README.md'
     )
