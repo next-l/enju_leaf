086469e0fbb89f1eb30376d08dba1a46a7ef2219
Kosuke Tanabe <nabeta@fastmail.fm>
2026-08-12 17:07:20 +0900

    ロケールの情報をセッションCookieに保存するよう修正 (#2068)
    * store locale in the session cookie
    
    * fix tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,21 @@ class ApplicationController < ActionController::Base
   private
 
   def switch_locale(&action)
-    locale = params[:locale] || I18n.locale
+    locale = params[:locale] || session[:locale] || I18n.locale
+    session[:locale] = locale
     I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    return super unless request.env["HTTP_ACCEPT_LANGUAGE"]
+
+    requested_locale = params[:locale] || session[:locale]
+    locale = I18n.available_locales.map(&:to_s).include?(requested_locale.to_s) ? requested_locale : nil
+
+    if locale && locale != request.env["HTTP_ACCEPT_LANGUAGE"].scan(/^[a-z]{2}/)[0]
+      { locale: locale }
+    else
+      super
+    end
   end
 end
